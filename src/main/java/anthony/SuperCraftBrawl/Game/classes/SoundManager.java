3a15940808c4e83572d69fb094a8f3337ab3ec16
NatanSambato <32022776+NatanSambato@users.noreply.github.com>
2023-10-14 17:22:15 -0300

    New SoundManager Helper class - Created a separated class to manage playing sounds to a single or more players

---
+++ src/main/java/anthony/SuperCraftBrawl/Game/classes/SoundManager.java
@@ -0,0 +1,41 @@
+package anthony.SuperCraftBrawl.Game.classes;
+
+import anthony.SuperCraftBrawl.Game.GameInstance;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+
+public class SoundManager {
+
+    // Play a sound to each game player from a specific player location
+    public static void playSoundToAllPlayersFromAPlayerLocation(GameInstance gameInstance, Player player, Sound sound, float volume, float pitch) {
+        for (Player gamePlayer : gameInstance.players) {
+            gamePlayer.playSound(player.getLocation(), sound, volume, pitch);
+        }
+    }
+
+    // Play a sound to each game player on its own location
+    public static void playSoundToAllPlayersOnEachPlayerLocation(GameInstance gameInstance, Sound sound, float volume, float pitch) {
+        for (Player gamePlayer : gameInstance.players) {
+            gamePlayer.playSound(gamePlayer.getLocation(), sound, volume, pitch);
+        }
+    }
+
+    // Play a sound to a single player
+    public static void playSoundToSinglePlayer(Player player, Sound sound, float volume, float pitch) {
+        player.playSound(player.getLocation(), sound, volume, pitch);
+    }
+
+    // Play the Successful Hit sound to a single player (Volume = 1; Pitch = 1)
+    public static void playSoundSuccessfulHitToSinglePlayer(Player player) {
+        player.playSound(player.getLocation(), Sound.SUCCESSFUL_HIT, 1, 1);
+    }
+
+    // Play the Successful Hit sound to a single player
+    public static void playSoundSuccessfulHitToSinglePlayer(Player player, float volume, float pitch) {
+        player.playSound(player.getLocation(), Sound.SUCCESSFUL_HIT, volume, pitch);
+    }
+}
+
+
+
+
