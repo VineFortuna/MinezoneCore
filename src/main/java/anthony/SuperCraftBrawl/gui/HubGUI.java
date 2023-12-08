784d83b36a78a0037423bd9a39129ad73f5a34c0
NatanSambato <32022776+NatanSambato@users.noreply.github.com>
2023-12-08 00:42:36 -0300

    1. Renamed "Normal" gameType to "Classic" 2. Updated ActiveGamesGUI (Quick Join GUI) to show a specific item for each gameType 3. Updated HubGUI (Game Selector GUI) a) Added joining a game in any map through the GUI by left-clicking b) Added join a random map feature by right-clicking the due gameType button

--- src/main/java/anthony/SuperCraftBrawl/gui/HubGUI.java
+++ src/main/java/anthony/SuperCraftBrawl/gui/HubGUI.java
@@ -5,15 +5,17 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Random;
 
+import anthony.SuperCraftBrawl.ChatColorHelper;
+import anthony.SuperCraftBrawl.Game.GameInstance;
+import anthony.SuperCraftBrawl.Game.GameType;
+import anthony.SuperCraftBrawl.Game.map.Maps;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.messaging.PluginMessageListener;
 
 import anthony.SuperCraftBrawl.ItemHelper;
 import anthony.SuperCraftBrawl.Core;
@@ -30,7 +32,7 @@ public class HubGUI implements InventoryProvider {
 	public SmartInventory inv;
 
 	public HubGUI(Core main) {
-		inv = SmartInventory.builder().id("myInventory").provider(this).size(3, 9)
+		inv = SmartInventory.builder().id("myInventory").provider(this).size(4, 9)
 				.title("" + ChatColor.DARK_GRAY + ChatColor.BOLD + "Game Selector").build();
 		this.main = main;
 
@@ -40,47 +42,135 @@ public class HubGUI implements InventoryProvider {
 	public void init(Player player, InventoryContents contents) {
 		int skywarsCount = GameManager.playercount.getOrDefault("sw-1", 0);
 
-		/*contents.set(1, 5,
-				ClickableItem.of(ItemHelper.setDetails(new ItemStack(Material.BOW),
-						"" + ChatColor.RESET + ChatColor.GREEN + "SkyWars " + ChatColor.DARK_GREEN + ChatColor.BOLD
-								+ "NEW GAME!",
-						"" + ChatColor.GRAY + "Loot up, build to center & ", ChatColor.GRAY + "claim the #1 spot!", "",
-						main.color("&e&lPlayers: &e" + skywarsCount)), e -> {
+//		contents.set(1, 5,
+//				ClickableItem.of(ItemHelper.setDetails(new ItemStack(Material.BOW),
+//						"" + ChatColor.RESET + ChatColor.GREEN + "SkyWars " + ChatColor.DARK_GREEN + ChatColor.BOLD
+//								+ "NEW GAME!",
+//						"" + ChatColor.GRAY + "Loot up, build to center & ", ChatColor.GRAY + "claim the #1 spot!", "",
+//						main.color("&e&lPlayers: &e" + skywarsCount)), e -> {
+//							inv.close(player);
+//							ByteArrayOutputStream b = new ByteArrayOutputStream();
+//							DataOutputStream out = new DataOutputStream(b);
+//
+//							try {
+//								out.writeUTF("Connect");
+//								out.writeUTF("sw-1");
+//								player.sendMessage(main.color("&e&l(!) &rConnecting to &esw-1"));
+//							} catch (Exception ex) {
+//								player.sendMessage(main.color("&c&l(!) &rThere was a problem connecting to &esw-1"));
+//							}
+//							player.sendPluginMessage(main, "BungeeCord", b.toByteArray());
+//						}));
+
+		int scbCount = GameManager.playercount.getOrDefault("scb-1", 0)
+				+ GameManager.playercount.getOrDefault("scb-2", 0);
+
+		// Classic Mode Button
+		ItemStack skullPlayer = ItemHelper.createSkullHeadPlayer(1, player.getName());
+
+		contents.set(1, 2,
+				ClickableItem.of(
+						ItemHelper.setDetails(skullPlayer,
+								"&eClassic",
+								"&7Choose a class, kill everyone",
+								"",
+								"&eRight click to join a random map"),
+						e -> {
+							// If item was Left-clicked opens GUI to choose map
+							if (e.isLeftClick()) {
+								new ClassicModeGUI(main).inv.open(player);
+							// If item was Right-clicked join random game
+							} else if (e.isRightClick()) {
+								player.sendMessage("yo bitch");
+
+								main.getGameManager().JoinMap(player, randomizeMap(GameType.CLASSIC));
+							}
+						}));
+
+		// Duels Mode Button
+		contents.set(1, 4,
+				ClickableItem.of(
+						ItemHelper.setDetails(new ItemStack(Material.IRON_SWORD),
+								"&eDuels",
+								"&71v1 someone until death",
+								"",
+								"&eRight click to join a random map"),
+						e -> {
+							// If item was Left-clicked opens GUI to choose map
+							if (e.isLeftClick()) {
+								new DuelsModeGUI(main).inv.open(player);
+							// If item was Right-clicked join random game
+							} else if (e.isRightClick()) {
+								main.getGameManager().JoinMap(player, randomizeMap(GameType.DUEL));
+							}
+						}));
+
+		// Frenzy Mode Button
+		contents.set(1, 6,
+				ClickableItem.of(
+						ItemHelper.setDetails(new ItemStack(Material.TNT),
+								"&eFrenzy",
+								"&7Random classes, big maps",
+								"",
+								"&eRight click to join a random map"),
+						e -> {
+							// If item was Left-clicked opens GUI to choose map
+							if (e.isLeftClick()) {
+								new FrenzyModeGUI(main).inv.open(player);
+							// If item was Right-clicked join random game
+							} else if (e.isRightClick()) {
+								main.getGameManager().JoinMap(player, randomizeMap(GameType.FRENZY));
+							}
+						}));
+
+		// SCB Duos Button
+		contents.set(3, 4,
+				ClickableItem.of(
+						ItemHelper.setDetails(new ItemStack(Material.DIAMOND_SWORD),
+								"&eDuos SCB",
+								"&7SCB with teammates",
+								"&e&lPlayers: &e" + GameManager.playercount.getOrDefault("scb-2", 0)),
+						e -> {
 							inv.close(player);
+
+							// Sending player to SCB Duos
+							Bukkit.getMessenger().registerOutgoingPluginChannel(main, "BungeeCord");
+
 							ByteArrayOutputStream b = new ByteArrayOutputStream();
 							DataOutputStream out = new DataOutputStream(b);
 
 							try {
 								out.writeUTF("Connect");
-								out.writeUTF("sw-1");
-								player.sendMessage(main.color("&e&l(!) &rConnecting to &esw-1"));
+								out.writeUTF("scb-2");
+								player.sendMessage(ChatColorHelper.color("&e&l(!) &rConnecting to &escb-2"));
 							} catch (Exception ex) {
-								player.sendMessage(main.color("&c&l(!) &rThere was a problem connecting to &esw-1"));
+								player.sendMessage(ChatColorHelper.color("&c&l(!) &rThere was a problem connecting to &escb-2"));
 							}
 							player.sendPluginMessage(main, "BungeeCord", b.toByteArray());
-						}));*/
-
-		int scbCount = GameManager.playercount.getOrDefault("scb-1", 0)
-				+ GameManager.playercount.getOrDefault("scb-2", 0);
-		contents.set(1, 3,
-				ClickableItem.of(
-						ItemHelper.setDetails(new ItemStack(Material.IRON_SWORD),
-								"" + ChatColor.RESET + ChatColor.GREEN + "SuperCraftBlocks " + ChatColor.DARK_GREEN
-										+ ChatColor.BOLD + "NEW UPDATE!",
-								"" + ChatColor.GRAY + "Choose a class, fight, & ",
-								ChatColor.GRAY + "claim the #1 spot!", "", main.color("&e&lPlayers: &e" + scbCount)),
-						e -> {
-							new SCBMenuGUI(main).inv.open(player);
 						}));
+				}
 
-		ItemStack stats = new ItemStack(Material.SKULL_ITEM, 1, (byte) 3);
-		SkullMeta statsMeta = (SkullMeta) stats.getItemMeta();
-		statsMeta.setOwner(player.getName());
-		stats.setItemMeta(statsMeta);
-	}
+		@Override
+		public void update(Player player, InventoryContents contents) {
+		}
 
-	@Override
-	public void update(Player player, InventoryContents contents) {
+		public Maps randomizeMap(GameType gameType) {
+			Maps randomizedMap;
+			boolean joined = false;
 
-	}
+			do {
+				Random random = new Random();
+				int randomizedNumber = random.nextInt(Maps.getGameType(gameType).size());
+
+				randomizedMap = Maps.getGameType(gameType).get(randomizedNumber);
+				GameInstance gameInstance = main.getGameManager().getInstanceOfMap(randomizedMap);
+
+				// If gameInstance does not exist
+				if (gameInstance == null) {
+					joined = true;
+				}
+			} while (!joined);
+
+			return randomizedMap;
+		}
 }
