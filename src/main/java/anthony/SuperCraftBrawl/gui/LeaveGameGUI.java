6fb866796ced855dfe0a2bc47086fcd1babf3ce0
NatanSambato <32022776+NatanSambato@users.noreply.github.com>
2023-12-25 00:40:49 -0300

    Created generic ConfirmationGUI - Renamed QuitGUI.java to LeaveGameGUI - LeaveGameGUI now extends ConfirmationGUI

---
+++ src/main/java/anthony/SuperCraftBrawl/gui/LeaveGameGUI.java
@@ -0,0 +1,10 @@
+package anthony.SuperCraftBrawl.gui;
+
+import anthony.SuperCraftBrawl.Core;
+
+public class LeaveGameGUI extends ConfirmationGUI {
+
+	public LeaveGameGUI(Core main) {
+		super(main, "Leave game?");
+	}
+}
