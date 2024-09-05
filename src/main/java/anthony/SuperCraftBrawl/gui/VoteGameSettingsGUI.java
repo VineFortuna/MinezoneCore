52e972d0406a5deb426c44c5a5e17ae46eefde6d
MinezoneMC <136758983+MinezoneMC@users.noreply.github.com>
2024-09-05 15:46:27 -0400

    Made game scoreboard display the game type and also on join message advertised Fishing

--- src/main/java/anthony/SuperCraftBrawl/gui/VoteGameSettingsGUI.java
+++ src/main/java/anthony/SuperCraftBrawl/gui/VoteGameSettingsGUI.java
@@ -36,82 +36,70 @@ public class VoteGameSettingsGUI implements InventoryProvider {
 		GameInstance game = main.getGameManager().GetInstanceOfPlayer(player);
 
 		if (game != null && game.state == GameState.WAITING && game.players.size() >= 2) {
-			addVoteGameStartButton(contents, player, data, game);
-			addVoteTimeButton(contents, player, data, game);
+			addVoteGameStartButton(contents, player, game);
+			addVoteTimeButton(contents, player, game);
 			addVoteGameTypeButton(contents, player, data, game);
 		}
 	}
 
-	private void addVoteGameStartButton(InventoryContents contents, Player player, PlayerData data, GameInstance game) {
+	/**
+	 * Adds a "Vote for Game Start" item to the specified inventory.
+	 * 
+	 * @param contents The inventory contents where the item will be added.
+	 * @param player The player who is interacting with the inventory.
+	 * @param game The current game instance in which the player is involved.
+	 */
+	private void addVoteGameStartButton(InventoryContents contents, Player player, GameInstance game) {
 		ItemStack voteGameStart = ItemHelper.setDetails(new ItemStack(Material.BEACON), ChatColor.YELLOW + "Game Start",
-				"", "(" + (game != null ? game.getGameSettings().totalStartVotes : "0") + "/"
+				"", "" + ChatColor.RESET + "(" + (game != null ? game.getGameSettings().totalStartVotes : "0") + "/"
 						+ (game != null ? game.players.size() : "0") + ")");
 		contents.set(1, 3, ClickableItem.of(voteGameStart, event -> {
 			if (event.getWhoClicked() instanceof Player) {
 				Player clickingPlayer = (Player) event.getWhoClicked();
 				inv.close(clickingPlayer);
-				handleVoteGameStart(clickingPlayer, data, game);
+				game.getGameSettings().handleVoteGameStart(clickingPlayer, game);
 			}
 		}));
 	}
 
-	private void addVoteTimeButton(InventoryContents contents, Player player, PlayerData data, GameInstance game) {
+	/**
+	 * Adds a "Vote for Time of Day" item to the specified inventory.
+	 * 
+	 * @param contents The inventory contents where the item will be added.
+	 * @param player The player who is interacting with the inventory.
+	 * @param game The current game instance in which the player is involved.
+	 */
+	private void addVoteTimeButton(InventoryContents contents, Player player, GameInstance game) {
 		ItemStack voteTime = ItemHelper.setDetails(new ItemStack(Material.WATCH),
 				ChatColor.YELLOW + "Time of Day -> Night", "",
-				"(" + (game != null ? game.getGameSettings().totalTimeVotes : "0") + "/"
-						+ (game != null ? game.players.size() : "0") + ")");
+				"" + ChatColor.RED + "NOT AVAILABLE");
 		contents.set(1, 5, ClickableItem.of(voteTime, event -> {
 			if (event.getWhoClicked() instanceof Player) {
-				Player clickingPlayer = (Player) event.getWhoClicked();
-				inv.close(clickingPlayer);
+				//Player clickingPlayer = (Player) event.getWhoClicked();
+				//inv.close(clickingPlayer);
 			}
 		}));
 	}
 
+	/**
+	 * Adds a "Vote for Game Type" item to the specified inventory.
+	 * 
+	 * @param contents The inventory contents where the item will be added.
+	 * @param player The player who is interacting with the inventory.
+	 * @param game The current game instance in which the player is involved.
+	 */
 	private void addVoteGameTypeButton(InventoryContents contents, Player player, PlayerData data, GameInstance game) {
-		ItemStack voteGameType = ItemHelper.setDetails(new ItemStack(Material.BEACON),
+		ItemStack voteGameType = ItemHelper.setDetails(new ItemStack(Material.TNT),
 				ChatColor.YELLOW + "Vote Game Type -> Frenzy", "",
-				"(" + (game != null ? game.getGameSettings().totalGameTypeVotes : "0") + "/"
+				"" + ChatColor.RESET + "(" + (game != null ? game.getGameSettings().totalGameTypeVotes : "0") + "/"
 						+ (game != null ? game.players.size() : "0") + ")");
 		contents.set(2, 4, ClickableItem.of(voteGameType, event -> {
 			if (event.getWhoClicked() instanceof Player) {
 				Player clickingPlayer = (Player) event.getWhoClicked();
 				inv.close(clickingPlayer);
-				handleVoteGameType(game);
+				game.getGameSettings().handleVoteGameType(player, game);
 			}
 		}));
-	}
-
-	private void handleVoteGameStart(Player player, PlayerData playerData, GameInstance game) {
-		GameSettings gs = null;
-
-		if (game != null && game.getGameSettings() != null) {
-			gs = game.getGameSettings();
-			if (!(gs.startVotes.contains(player))) {
-				gs.totalStartVotes++;
-				gs.startVotes.add(player);
-				updateGameStartVoteStatus(player, game, ChatColor.GREEN);
-			} else {
-				gs.totalStartVotes--;
-				gs.startVotes.remove(player);
-				updateGameStartVoteStatus(player, game, ChatColor.RED);
-			}
-		}
-	}
-
-	private void handleVoteGameType(GameInstance game) {
-		
-	}
-
-	private void updateGameStartVoteStatus(Player player, GameInstance game, ChatColor color) {
-		String status = color + (color == ChatColor.GREEN ? " is Ready " : " is no longer Ready ");
-		String message = ChatColor.DARK_GREEN + ChatColor.BOLD.toString() + "(!) " + ChatColor.RESET + ChatColor.YELLOW
-				+ player.getName() + ChatColor.BOLD + status + ChatColor.RED + "(" + ChatColor.GREEN
-				+ game.getGameSettings().totalStartVotes + "/" + game.players.size() + ChatColor.RED + ")";
-		game.TellAll(message);
-
-		if (game.getGameSettings().totalStartVotes == game.players.size())
-			game.getGameSettings().forceStartGame();
 	}
 
 	@Override
