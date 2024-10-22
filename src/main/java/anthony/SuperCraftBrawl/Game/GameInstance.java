7e0a8ce6f3afe4ceae479d801c146c07b6e61ab6
NatanSambato <32022776+NatanSambato@users.noreply.github.com>
2024-10-21 21:00:36 -0300

    # Conflicts: #	src/main/java/anthony/SuperCraftBrawl/Game/GameSettings.java #	src/main/java/anthony/SuperCraftBrawl/Game/map/Maps.java #	src/main/java/anthony/SuperCraftBrawl/gui/GenericModeGUI.java

--- src/main/java/anthony/SuperCraftBrawl/Game/GameInstance.java
+++ src/main/java/anthony/SuperCraftBrawl/Game/GameInstance.java
@@ -80,6 +80,7 @@ public class GameInstance {
 	public Player firstBlood;
 	private final Map<UUID, Location> lastKnownLocations = new HashMap<>();
 	public List<ItemStack> allItemDrops = new ArrayList<>();
+	public List<ItemStack> items = new ArrayList<>();
 	public List<Player> favClassSelection = new ArrayList<>();
 
 	// Constructors:
@@ -97,6 +98,7 @@ public class GameInstance {
 		oldClasses = new HashMap<>();
 		allClasses = new HashMap<>();
 		initialiseMap();
+		createItemDrops();
 	}
 
 	public GameInstance(GameManager gameManager, DuosMaps map) {
@@ -860,6 +862,10 @@ public class GameInstance {
 	}
 
 	public ItemStack getItemToDrop() {
+		return items.get(random.nextInt(items.size()));
+	}
+
+	private void createItemDrops() {
 		// Slowness Potion
 		ItemStack slownessPot = ItemHelper.createPotionItem(PotionType.SLOWNESS, 1, 15, true, true, true);
 		ItemHelper.setDetails(slownessPot, "&7&lSLOWNESS II &7(15 sec)");
@@ -878,11 +884,17 @@ public class GameInstance {
 
 		// Bomb
 		ItemStack bomb = ItemHelper.createPotionItem(PotionType.INSTANT_DAMAGE, 1000, 0, true, true, true);
-		ItemHelper.setDetails(bomb, "&4&lBOMB", "&7Be careful!", "&7Instantly kills... anyone");
+		ItemHelper.setDetails(bomb,
+				"&4&lBOMB",
+				"&7Be careful!",
+				"&7Instantly kills... anyone"
+		);
 
 		// Brooms
-		ItemStack broom = ItemHelper.setDetails(new ItemStack(Material.WHEAT, 4), "&5&lBROOM",
-				"&7Sends you up and saves you from the void");
+		ItemStack broom = ItemHelper.setDetails(new ItemStack(Material.WHEAT, 4),
+				"&5&lBROOM",
+				"&7Sends you up and saves you from the void"
+		);
 
 		// Hammer
 		ItemStack hammer = ItemHelper.setDetails(new ItemStack(Material.IRON_SWORD, 1, (short) 250), "&d&lHAMMER");
@@ -895,24 +907,35 @@ public class GameInstance {
 		ItemStack pearl = ItemHelper.setDetails(new ItemStack(Material.ENDER_PEARL), "&5&lENDER PEARL");
 
 		// Slowballs
-		ItemStack slowballs = ItemHelper.setDetails(new ItemStack(Material.SNOW_BALL, 8), "&f&lSLOWBALLS",
-				"&7Give Slowness 1 for 3s to an enemy");
+		ItemStack slowballs = ItemHelper.setDetails(new ItemStack(Material.SNOW_BALL, 8),
+				"&f&lSLOWBALLS",
+				"&7Give Slowness 1 for 3s to an enemy"
+		);
 
 		// Mini Shield
-		ItemStack miniShield = ItemHelper.setDetails(new ItemStack(Material.POTION, 1), "&9&lMINI-SHIELD",
-				"&7Absorption 1");
+		ItemStack miniShield = ItemHelper.setDetails(new ItemStack(Material.POTION, 1),
+				"&9&lMINI-SHIELD",
+				"&7Absorption 1"
+		);
 
 		// Bounty
-		ItemStack bounty = ItemHelper.setDetails(new ItemStack(Material.NETHER_STAR, 1), "&a&lBOUNTY",
-				"&7Set a bounty on a random player", "&7Kill them to claim extra tokens");
+		ItemStack bounty = ItemHelper.setDetails(new ItemStack(Material.NETHER_STAR, 1),
+				"&a&lBOUNTY",
+				"&7Set a bounty on a random player",
+				"&7Kill them to claim extra tokens"
+		);
 
 		// Blooper
-		ItemStack blooper = ItemHelper.setDetails(new ItemStack(Material.RABBIT_FOOT), "&e&lBLOOPER",
-				"&7Give Blindness or Nausea to an enemy");
+		ItemStack blooper = ItemHelper.setDetails(new ItemStack(Material.RABBIT_FOOT),
+				"&e&lBLOOPER",
+				"&7Give Blindness or Nausea to an enemy"
+		);
 
 		// Nuke
-		ItemStack nuke = ItemHelper.setDetails(new ItemStack(Material.TNT, 3), "&4&lNUKE",
-				"&7Spawn TNTs where you're looking");
+		ItemStack nuke = ItemHelper.setDetails(new ItemStack(Material.TNT, 3),
+				"&4&lNUKE",
+				"&7Spawn TNTs where you're looking"
+		);
 		ItemHelper.setGlowing(nuke, true);
 
 		// Instagib
@@ -934,15 +957,16 @@ public class GameInstance {
 		ItemStack creeperEgg = ItemHelper.createMonsterEgg(EntityType.CREEPER, 1, "&a&lCREEPER POKEBALL");
 
 		// Milk Bucket
-		ItemStack milk = ItemHelper.setDetails(new ItemStack(Material.MILK_BUCKET), "&f&lMILK",
-				"&7Removes fire and all negative effects");
+		ItemStack milk = ItemHelper.setDetails(new ItemStack(Material.MILK_BUCKET),
+				"&f&lMILK",
+				"&7Removes fire and all negative effects"
+		);
 
 		// Golden Apple
 		ItemStack goldenApple = ItemHelper.setDetails(new ItemStack(Material.GOLDEN_APPLE), "&6&lGOLDEN APPLE");
 
 		// Notch Apple
-		ItemStack notchApple = ItemHelper.create(Material.GOLDEN_APPLE, "&d&lNOTCH APPLE");
-		notchApple.setDurability((short) 1);
+		ItemStack notchApple = ItemHelper.setDetails(new ItemStack(Material.GOLDEN_APPLE, 1, (short) 1), "&d&lNOTCH APPLE");
 
 		allItemDrops.add(slownessPot);
 		allItemDrops.add(healthPot);
@@ -968,14 +992,13 @@ public class GameInstance {
 		allItemDrops.add(goldenApple);
 		allItemDrops.add(notchApple);
 
-		List<ItemStack> items = Arrays.asList(notchApple,
-				slownessPot, bazooka, bazooka, healthPot, speedPot, slownessPot, slownessPot, speedPot, bazooka,
-				goldenApple, hammer, healthPot, extraLife, healthPot, milk, milk, milk, blooper, blooper, blooper,
-				blooper, nuke, nuke, nuke, nuke, nuke, bomb, pearl, pearl, miniShield, miniShield, slowballs, slowballs,
-				slowballs, fireRes, fireRes, instagib, instagib, instagib, broom, broom, zombieEgg, zombieEgg,
-				zombieEgg, skeletonEgg, skeletonEgg, witchEgg, bounty, creeperEgg, creeperEgg);
-
-		return items.get(random.nextInt(items.size()));
+		items = Arrays.asList(
+				goldenApple, notchApple, slownessPot, bazooka, bazooka, healthPot, speedPot, slownessPot, slownessPot,
+				speedPot, bazooka, goldenApple, hammer, healthPot, extraLife, healthPot, milk, milk, milk, blooper,
+				blooper, blooper, blooper, nuke, nuke, nuke, nuke, nuke, bomb, pearl, pearl, miniShield, miniShield,
+				slowballs, slowballs, slowballs, fireRes, fireRes, instagib, instagib, instagib, broom, broom,
+				zombieEgg, zombieEgg, 	zombieEgg, skeletonEgg, skeletonEgg, witchEgg, bounty, creeperEgg, creeperEgg
+		);
 	}
 
 	public String truncateString(String string, int length) {
@@ -2364,7 +2387,7 @@ public class GameInstance {
 
 		return false;
 	}
-	
+
 	public void clearLastPosition(Player player) {
 		UUID playerId = player.getUniqueId();
 		lastKnownLocations.remove(playerId);
