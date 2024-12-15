0fed27c0e2ea79d3f1f5b176b111c33d45114581
MinezoneMC <136758983+MinezoneMC@users.noreply.github.com>
2024-12-15 18:26:03 -0500

    Finished christmas rewards

--- src/main/java/anthony/SuperCraftBrawl/gui/cosmetics/DeathEffectsGUI.java
+++ src/main/java/anthony/SuperCraftBrawl/gui/cosmetics/DeathEffectsGUI.java
@@ -18,13 +18,8 @@ public class DeathEffectsGUI implements InventoryProvider {
 	public SmartInventory inv;
 
 	public DeathEffectsGUI(Core main, SmartInventory parent) {
-		inv = SmartInventory.builder()
-				.id("myInventory")
-				.provider(this)
-				.size(3, 9)
-				.title("" + ChatColor.DARK_GRAY + ChatColor.BOLD + "Death Effects")
-				.parent(parent)
-				.build();
+		inv = SmartInventory.builder().id("myInventory").provider(this).size(3, 9)
+				.title("" + ChatColor.DARK_GRAY + ChatColor.BOLD + "Death Effects").parent(parent).build();
 		this.main = main;
 	}
 
@@ -34,6 +29,7 @@ public class DeathEffectsGUI implements InventoryProvider {
 		data.redstone = 0;
 		data.web = 0;
 		data.bottleEXP = 0;
+		data.snowballDeathEffect = 0;
 	}
 
 	@Override
@@ -46,87 +42,81 @@ public class DeathEffectsGUI implements InventoryProvider {
 		ItemStack redstone = ItemHelper.create(Material.REDSTONE, org.bukkit.ChatColor.YELLOW + "Redstone");
 		ItemStack cobweb = ItemHelper.create(Material.WEB, org.bukkit.ChatColor.YELLOW + "Cobweb");
 		ItemStack expBottle = ItemHelper.create(Material.EXP_BOTTLE, org.bukkit.ChatColor.YELLOW + "Exp Bottle");
-
+		ItemStack snowball = ItemHelper.create(Material.EXP_BOTTLE, org.bukkit.ChatColor.YELLOW + "Snowball");
 
 		// Setting Items
-		contents.fillBorders(ClickableItem.of(ItemHelper.setDetails(new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 7), " "), e-> {}));
+		contents.fillBorders(ClickableItem
+				.of(ItemHelper.setDetails(new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 7), " "), e -> {
+				}));
 
 		if (data != null) {
 			// Golden Apple
-			contents.set(1, 1, ClickableItem.of(
-					goldenApple,
-					e -> {
-						if (data.goldApple == 0) {
-							this.resetData(data);
-							data.goldApple = 1;
-							player.sendMessage(main.color("&9&l(!) &rYou have enabled &eGolden Apple Death Particle"));
-						} else {
-							this.resetData(data);
-							player.sendMessage(main.color("&9&l(!) &rYou have disabled &eGolden Apple Death Particle"));
-						}
-					}));
+			contents.set(1, 1, ClickableItem.of(goldenApple, e -> {
+				if (data.goldApple == 0) {
+					this.resetData(data);
+					data.goldApple = 1;
+					player.sendMessage(main.color("&9&l(!) &rYou have enabled &eGolden Apple Death Particle"));
+				} else {
+					this.resetData(data);
+					player.sendMessage(main.color("&9&l(!) &rYou have disabled &eGolden Apple Death Particle"));
+				}
+			}));
 
 			// Glowstone
-			contents.set(1, 2, ClickableItem.of(
-					glowstone,
-					e -> {
-						if (data.glowstone == 0) {
-							this.resetData(data);
-							data.glowstone = 1;
-							player.sendMessage(main.color("&9&l(!) &rYou have enabled &eGlowstone Dust Death Particle"));
-						} else {
-							this.resetData(data);
-							player.sendMessage(main.color("&9&l(!) &rYou have disabled &eGlowstone Dust Death Particle"));
-						}
-					}));
+			contents.set(1, 2, ClickableItem.of(glowstone, e -> {
+				if (data.glowstone == 0) {
+					this.resetData(data);
+					data.glowstone = 1;
+					player.sendMessage(main.color("&9&l(!) &rYou have enabled &eGlowstone Dust Death Particle"));
+				} else {
+					this.resetData(data);
+					player.sendMessage(main.color("&9&l(!) &rYou have disabled &eGlowstone Dust Death Particle"));
+				}
+			}));
 
 			// Redstone
-			contents.set(1, 3, ClickableItem.of(
-					redstone,
-					e -> {
-						if (data.redstone == 0) {
-							this.resetData(data);
-							data.redstone = 1;
-							player.sendMessage(main.color("&9&l(!) &rYou have enabled &eRedstone Death Particle"));
-						} else {
-							this.resetData(data);
-							player.sendMessage(main.color("&9&l(!) &rYou have disabled &eRedstone Death Particle"));
-						}
-					}));
+			contents.set(1, 3, ClickableItem.of(redstone, e -> {
+				if (data.redstone == 0) {
+					this.resetData(data);
+					data.redstone = 1;
+					player.sendMessage(main.color("&9&l(!) &rYou have enabled &eRedstone Death Particle"));
+				} else {
+					this.resetData(data);
+					player.sendMessage(main.color("&9&l(!) &rYou have disabled &eRedstone Death Particle"));
+				}
+			}));
 
 			// Cobweb
-			contents.set(1, 4, ClickableItem.of(
-					cobweb,
-					e -> {
-						if (data.web == 0) {
-							this.resetData(data);
-							data.web = 1;
-							player.sendMessage(main.color("&9&l(!) &rYou have enabled &eCobweb Death Particle"));
-						} else {
-							this.resetData(data);
-							player.sendMessage(main.color("&9&l(!) &rYou have disabled &eCobweb Death Particle"));
-						}
-					}));
+			contents.set(1, 4, ClickableItem.of(cobweb, e -> {
+				if (data.web == 0) {
+					this.resetData(data);
+					data.web = 1;
+					player.sendMessage(main.color("&9&l(!) &rYou have enabled &eCobweb Death Particle"));
+				} else {
+					this.resetData(data);
+					player.sendMessage(main.color("&9&l(!) &rYou have disabled &eCobweb Death Particle"));
+				}
+			}));
 
 			// Exp Bottle
-			contents.set(1, 5, ClickableItem.of(
-					expBottle,
-					e -> {
-						if (data.bottleEXP == 0) {
-							this.resetData(data);
-							data.bottleEXP = 1;
-							player.sendMessage(main.color("&9&l(!) &rYou have enabled &eBottle o' enchanting Death Particle"));
-						} else {
-							this.resetData(data);
-							player.sendMessage(main.color("&9&l(!) &rYou have disabled &eBottle o' enchanting Death Particle"));
-						}
-					}));
+			contents.set(1, 5, ClickableItem.of(expBottle, e -> {
+				if (data.bottleEXP == 0) {
+					this.resetData(data);
+					data.bottleEXP = 1;
+					player.sendMessage(main.color("&9&l(!) &rYou have enabled &eBottle o' enchanting Death Particle"));
+				} else {
+					this.resetData(data);
+					player.sendMessage(main.color("&9&l(!) &rYou have disabled &eBottle o' enchanting Death Particle"));
+				}
+			}));
+			contents.set(1, 6, ClickableItem.of(snowball, e -> {
+				player.sendMessage(main.color("&c&l(!) &rYou have not unlocked this yet!"));
+			}));
 
-			contents.set(2, 8, ClickableItem.of(
-					ItemHelper.setDetails(new ItemStack(Material.ARROW), ChatColor.GRAY + "Go Back"), e -> {
+			contents.set(2, 8, ClickableItem
+					.of(ItemHelper.setDetails(new ItemStack(Material.ARROW), ChatColor.GRAY + "Go Back"), e -> {
 						inv.getParent().get().open(player);
-					}
-			));
+					}));
 		}
 	}
 
--- src/main/java/anthony/SuperCraftBrawl/gui/cosmetics/GadgetsGUI.java
+++ src/main/java/anthony/SuperCraftBrawl/gui/cosmetics/GadgetsGUI.java
@@ -1,19 +1,29 @@
 package anthony.SuperCraftBrawl.gui.cosmetics;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+
 import anthony.SuperCraftBrawl.Core;
-import anthony.util.ItemHelper;
 import anthony.SuperCraftBrawl.playerdata.PlayerData;
+import anthony.util.ItemHelper;
 import fr.minuskube.inv.ClickableItem;
 import fr.minuskube.inv.SmartInventory;
 import fr.minuskube.inv.content.InventoryContents;
 import fr.minuskube.inv.content.InventoryProvider;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class GadgetsGUI implements InventoryProvider {
 
@@ -59,18 +69,21 @@ public class GadgetsGUI implements InventoryProvider {
 		ItemStack fishingRod = main.getFishingRod(player);
 
 		List<String> snow = new ArrayList<>();
-		snow.add(ChatColor.DARK_GRAY + "Snow Particles");
-		snow.add("");
-		snow.add(main.color("&e&lUNLOCKED"));
 		ItemStack snowball = ItemHelper.create(Material.SNOW_BALL,
-				ChatColor.YELLOW.toString() + ChatColor.BOLD + "Christmas Particles", snow);
-		
+				main.color("&r&lSnow Particles"), snow);
+
 		List<String> snowman = new ArrayList<>();
 		snow.add(ChatColor.DARK_GRAY + "Snowman Pet");
 		snow.add("");
 		snow.add(main.color("&e&lUNLOCKED"));
 		ItemStack snowmanPet = ItemHelper.create(Material.MONSTER_EGG,
 				ChatColor.YELLOW.toString() + ChatColor.BOLD + "Snowman Pet", snowman);
+
+		
+		List<String> candyCaneSwirl = new ArrayList<>();
+		candyCaneSwirl.add(main.color("&c&lCandy &r&lCane &c&lSwirl"));
+		ItemStack candyCane = ItemHelper.createSkullTexture(
+				"e3RleHR1cmVzOntTS0lOOnt1cmw6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMWM4M2E0MmU4MmNkNmE3MGUyMTZkOWE4YzJmZjZmMWU1ZTViMjU2Y2VhM2I4Y2QyMjU0NzIzOTNhYTNlY2E1YSJ9fX0=");
 
 		// Setting Items
 		contents.fillBorders(ClickableItem
@@ -166,26 +179,93 @@ public class GadgetsGUI implements InventoryProvider {
 				player.sendMessage(main.color("&c&l(!) &rYou have not unlocked this gadget yet!"));
 			}
 		}));
-		
+
 		contents.set(1, 6, ClickableItem.of(snowmanPet, e -> {
-			//if (data.snowParticles == 1) {
-				if (!(main.getListener().snowmanPetPlayers.contains(player))) {
-					player.sendMessage(main.color("&r&l(!) &rYou equipped &eSnowman &rpet"));
-					main.getListener().snowmanPetPlayers.add(player);
-					main.getListener().snowmanPet(player);
-				} else {
-					player.sendMessage(main.color("&r&l(!) &rYou removed &eSnowman &rpet"));
-					main.getListener().snowmanPetPlayers.remove(player);
-				}
-			//} else {
-				//player.sendMessage(main.color("&c&l(!) &rYou have not unlocked this gadget yet!"));
-			//}
+			// if (data.snowParticles == 1) {
+			/*if (!(main.getListener().snowmanPetPlayers.contains(player))) {
+				player.sendMessage(main.color("&r&l(!) &rYou equipped &eSnowman &rpet"));
+				main.getListener().snowmanPetPlayers.add(player);
+				main.getListener().snowmanPet(player);
+			} else {
+				player.sendMessage(main.color("&r&l(!) &rYou removed &eSnowman &rpet"));
+				main.getListener().snowmanPetPlayers.remove(player);
+			}*/
+			// } else {
+			player.sendMessage(main.color("&c&l(!) &rYou have not unlocked this gadget yet!"));
+			// }
+		}));
+
+		contents.set(1, 7, ClickableItem.of(candyCane, e -> {
+			// if (data.snowParticles == 1) {
+			/*if (!(main.getListener().candyCaneSwirlPlayers.contains(player))) {
+				player.sendMessage(main.color("&r&l(!) &rYou equipped &eCandy Cane Swirl &gadget"));
+				main.getListener().candyCaneSwirlPlayers.add(player);
+				main.getListener().candyCaneSwirlCosmetic(player);
+			} else {
+				player.sendMessage(main.color("&r&l(!) &rYou removed &eCandy Cane Swirl &gadget"));
+				main.getListener().candyCaneSwirlPlayers.remove(player);
+			}*/
+			// } else {
+			player.sendMessage(main.color("&c&l(!) &rYou have not unlocked this gadget yet!"));
+			// }
 		}));
 
 		contents.set(2, 8, ClickableItem
 				.of(ItemHelper.setDetails(new ItemStack(Material.ARROW), ChatColor.GRAY + "Go Back"), e -> {
 					inv.getParent().get().open(player);
 				}));
+	}
+
+//	//give @p minecraft:player_head[profile={id:[I;-853038783,-424126035,-1902419453,-1029693902],properties:[{name:"textures",value:"e3RleHR1cmVzOntTS0lOOnt1cmw6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWQwZmJjN2E2YWQ4M2U5MjRkYjZjYTBjYTM0N2RjZjVmMmY0MzRmMzQ3NDJmODMyOTYwYTA0MDZmYmRiYjE4NyJ9fX0="}]},minecraft:lore=['{"text":"https://namemc.com/skin/944218dcc2d0316a"}']]
+
+	private static final String ELF_TEXTURE = "e3RleHR1cmVzOntTS0lOOnt1cmw6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWQwZmJjN2E2YWQ4M2U5MjRkYjZjYTBjYTM0N2RjZjVmMmY0MzRmMzQ3NDJmODMyOTYwYTA0MDZmYmRiYjE4NyJ9fX0=";
+
+	private void equipElfSet(Player player) {
+		// Give the player the Elf head
+		ItemStack elfHead = getCustomSkull(ELF_TEXTURE);
+		// Give dyed leather armor
+		ItemStack chest = getDyedArmor(Material.LEATHER_CHESTPLATE, Color.GREEN, ChatColor.GREEN + "Elf Tunic");
+		ItemStack legs = getDyedArmor(Material.LEATHER_LEGGINGS, Color.RED, ChatColor.RED + "Elf Pants");
+		ItemStack boots = getDyedArmor(Material.LEATHER_BOOTS, Color.GREEN, ChatColor.GREEN + "Elf Boots");
+
+		player.getInventory().setHelmet(elfHead);
+		player.getInventory().setChestplate(chest);
+		player.getInventory().setLeggings(legs);
+		player.getInventory().setBoots(boots);
+	}
+
+	private ItemStack getDyedArmor(Material material, Color color, String name) {
+		ItemStack item = new ItemStack(material);
+		LeatherArmorMeta meta = (LeatherArmorMeta) item.getItemMeta();
+		meta.setColor(color);
+		meta.setDisplayName(name);
+		item.setItemMeta(meta);
+		return item;
+	}
+
+	/**
+	 * Creates a custom player head with the given base64 texture. Works on 1.8
+	 * using reflection and GameProfile.
+	 */
+	private ItemStack getCustomSkull(String base64) {
+		ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
+		SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
+
+		// Create a fake profile with the given texture
+		GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+		profile.getProperties().put("textures", new Property("textures", base64));
+
+		try {
+			Field profileField = skullMeta.getClass().getDeclaredField("profile");
+			profileField.setAccessible(true);
+			profileField.set(skullMeta, profile);
+		} catch (NoSuchFieldException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		skullMeta.setDisplayName(ChatColor.GREEN + "Elf Head");
+		skull.setItemMeta(skullMeta);
+		return skull;
 	}
 
 	@Override
