fd58947976bd838cfa8a22e379282dcf661265c8
Evstike <evstike@gmail.com>
2025-01-18 22:03:41 -0500

    Class balance

--- src/main/java/anthony/SuperCraftBrawl/Game/classes/all/BatClass.java
+++ src/main/java/anthony/SuperCraftBrawl/Game/classes/all/BatClass.java
@@ -24,7 +24,7 @@ public class BatClass extends BaseClass {
 				null,
 				"eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWU5OWRlZWY5MTlkYjY2YWMyYmQyOGQ2MzAyNzU2Y2NkNTdjN2Y4YjEyYjlkY2E4ZjQxYzNlMGEwNGFjMWNjIn19fQ==",
 				null,
-				6,
+				0,
 				"Bat"
 		);
 	}
