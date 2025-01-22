939957dbc4ee05049533a5c3c4a3e0a2abe896e0
Evstike <evstike@gmail.com>
2025-01-21 19:49:27 -0500

    Prevented adding duplicate fav classes

--- src/main/java/anthony/SuperCraftBrawl/gui/FreeClassesGUI.java
+++ src/main/java/anthony/SuperCraftBrawl/gui/FreeClassesGUI.java
@@ -82,7 +82,7 @@ public class FreeClassesGUI implements InventoryProvider {
 												main.getDataManager().saveData(data);
 											} else {
 												player.sendMessage(
-														main.color("&c&l(!) &r" + type.getTag() + " &ris already one of your favorites"));
+														main.color("&c&l(!) &r" + type.getTag() + " &ris already one of your favorites!"));
 											}
 										}
 									} else if (e.isLeftClick()) {
