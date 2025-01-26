5c0877a138b9f90265304712c8504c557280507f
Evstike <evstike@gmail.com>
2025-01-25 20:21:50 -0500

    Remade /color

--- src/main/java/anthony/SuperCraftBrawl/Game/classes/all/EnderdragonClass.java
+++ src/main/java/anthony/SuperCraftBrawl/Game/classes/all/EnderdragonClass.java
@@ -40,14 +40,14 @@ public class EnderdragonClass extends BaseClass {
 		playerInv.setItem(0, this.getAttackWeapon());
 		playerInv.setItem(1, ItemHelper.setDetails(new ItemStack(Material.ENDER_PEARL, 5),
 				"" + ChatColor.BLACK + ChatColor.BOLD + "Teleporters"));
-		player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 999999999, 0));
+		player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 999999999, 1));
 	}
 
 	@SuppressWarnings("unlikely-arg-type")
 	@Override
 	public void Tick(int gameTicks) {
 		if (!(player.getActivePotionEffects().contains(PotionEffectType.WEAKNESS)))
-			player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 999999999, 0));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 999999999, 1));
 
 		if (instance.classes.containsKey(player) && instance.classes.get(player).getType() == ClassType.Enderdragon
 				&& instance.classes.get(player).getLives() > 0) {
