4eaccae5df4067e7f68266c4b02b07b744f3cc43
Evstike <evstike@gmail.com>
2025-04-16 13:21:02 -0400

    Implemented checkpoints

--- src/main/java/anthony/parkour/ArenaInstance.java
+++ src/main/java/anthony/parkour/ArenaInstance.java
@@ -2,11 +2,14 @@ package anthony.parkour;
 
 import org.bukkit.util.Vector;
 
+import java.util.List;
+
 public class ArenaInstance {
 
 	public Vector startLoc = new Vector(0, 100, 0);
 	public Vector endLoc = new Vector(0, 100, 0);
 	public double boundsX, boundsY, boundsZ;
+	public List<Vector> checkpoints;
 	
 	public ArenaInstance setStartLoc(Vector v) {
 		this.startLoc = v;
@@ -22,6 +25,11 @@ public class ArenaInstance {
 		this.boundsX = boundsX;
 		this.boundsY = boundsY;
 		this.boundsZ = boundsZ;
+		return this;
+	}
+
+	public ArenaInstance setCheckpoints(List<Vector> checkpoints) {
+		this.checkpoints = checkpoints;
 		return this;
 	}
 
