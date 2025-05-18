560d163033e024d09c7930f0ab566abd8f3d8b76
NatanSambato <32022776+NatanSambato@users.noreply.github.com>
2025-05-18 14:38:01 -0300

    BrewingStand - Mini rework: - Weapon: wooden sword >> Sharpness 3 - Removed brewing cooldown - Removed one potion inventory cap - Disabled getting blaze powder while brewing - Removed Slowness potion - Changed potion costs and durations

--- src/main/java/anthony/SuperCraftBrawl/Game/classes/all/BrewingStandClass.java
+++ src/main/java/anthony/SuperCraftBrawl/Game/classes/all/BrewingStandClass.java
@@ -1,44 +1,43 @@
 package anthony.SuperCraftBrawl.Game.classes.all;
 
 import anthony.SuperCraftBrawl.Game.GameInstance;
+import anthony.SuperCraftBrawl.Game.classes.Ability;
 import anthony.SuperCraftBrawl.Game.classes.BaseClass;
 import anthony.SuperCraftBrawl.Game.classes.ClassType;
+import anthony.util.ChatColorHelper;
 import anthony.util.ItemHelper;
+import anthony.util.SoundManager;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.PotionSplashEvent;
-import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.potion.PotionType;
 import org.bukkit.scheduler.BukkitRunnable;
-
-import java.util.List;
 
 public class BrewingStandClass extends BaseClass {
 
-	ItemStack potionItem;
-	ItemStack resPotItem;
-	PotionEffect resistance = new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 15, 0, false, true); // Not using it to set the effect
+	private ItemStack weapon;
+	private ItemStack brewingItem;
+	private ItemStack barrierItem;
+	private final Ability brewAbility = new Ability("&e&lBrewing", 3, player);
+	private final PotionEffect jump = new PotionEffect(PotionEffectType.JUMP, 16 * 20, 7, true);
+	private final PotionEffect speed = new PotionEffect(PotionEffectType.SPEED, 12 * 20, 1, true);
+	private final PotionEffect strength = new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 8 * 20, 0, true);
+	private final PotionEffect resistance = new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 12 * 20, 0, false, true);
+	private final PotionEffect regeneration = new PotionEffect(PotionEffectType.REGENERATION, 5 * 20, 2, true);
 
-	private int cooldownSec;
-	private int cooldownDuration = 10 * 1000;
-	private boolean used = false;
+	String potionDisplayNamePrefix;
+	private boolean hasSentBrewedMessage;
+
 	private BukkitRunnable runnable;
 
 	public BrewingStandClass(GameInstance instance, Player player) {
@@ -53,6 +52,44 @@ public class BrewingStandClass extends BaseClass {
 				6,
 				"BrewingStand"
 		);
+
+		initializeItems();
+	}
+
+	private void initializeItems() {
+		// Weapon
+		weapon = ItemHelper.setDetails(
+				new ItemStack(Material.BLAZE_ROD),
+				"&e&lBlaze Core"
+		);
+		weapon.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 3);
+		weapon.addUnsafeEnchantment(Enchantment.KNOCKBACK, 1);
+
+		// Brewing Ability
+		String jumpString = "&a&oJump &e" + (jump.getAmplifier() + 1) + " &7for &e" + jump.getDuration() / 20 + "s";
+		String speedString = "&b&oSpeed &e" + (speed.getAmplifier() + 1) + " &7for &e" + speed.getDuration() / 20 + "s";
+		String regenString = "&d&oRegeneration &e" + (regeneration.getAmplifier() + 1) + " &7for &e" + regeneration.getDuration() / 20 + "s";
+		String strengthString = "&4&oStrength &e" + (strength.getAmplifier() + 1) + " &7for &e" + strength.getDuration() / 20 + "s";
+		String resistanceString = "&f&oResistance &e" + (resistance.getAmplifier() + 1) + " &7for &e" + resistance.getDuration() / 20 + "s";
+
+		brewingItem = ItemHelper.setDetails(
+				new ItemStack(Material.BREWING_STAND_ITEM),
+				brewAbility.getAbilityNameRightClickMessage(),
+				"&7Hit enemies to gain blaze powder",
+				"&7Use it to brew potions",
+				"",
+				"&71 Powder - " + jumpString,
+				"&72 Powder - " + speedString,
+				"&73 Powder - " + strengthString,
+				"&74 Powder - " + resistanceString,
+				"&75 Powder - " + regenString
+		);
+
+		// Barrier
+		barrierItem = ItemHelper.setDetails(
+				new ItemStack(Material.BARRIER),
+				"&cNo blaze powder yet!"
+		);
 	}
 
 	/*
@@ -60,14 +97,10 @@ public class BrewingStandClass extends BaseClass {
 	 */
 	@Override
 	public void SetItems(Inventory playerInv) {
-		this.used = false; // Reset each life Brewing Stand usage
-		alexBrewingStand.startTime = System.currentTimeMillis() - 100000; // To reset cooldown each life
-		playerInv.setItem(0, getAttackWeapon());
-		playerInv.setItem(1,
-				ItemHelper.setDetails(new ItemStack(Material.BARRIER), ChatColor.RED + "No potion item yet!"));
-		playerInv.setItem(2, getBrewingStand());
-		playerInv.setItem(8,
-				ItemHelper.setDetails(new ItemStack(Material.BARRIER), ChatColor.RED + "No blaze powder yet!"));
+		brewAbility.getCooldownInstance().reset();
+		playerInv.setItem(0, weapon);
+		playerInv.setItem(1, brewingItem);
+		playerInv.setItem(8, barrierItem);
 	}
 
 	/*
@@ -77,188 +110,167 @@ public class BrewingStandClass extends BaseClass {
 	 */
 	@Override
 	public void Tick(int gameTicks) {
-		if (instance.classes.containsKey(player) && instance.classes.get(player).getType() == ClassType.BrewingStand
-				&& instance.classes.get(player).getLives() > 0) {
-			if (!checkIfDead(player, instance))
-				checkIfEmpty();
-			this.cooldownSec = (this.cooldownDuration - alexBrewingStand.getTime()) / 1000 + 1;
-			cooldownActionBar(this.cooldownSec, this.cooldownDuration, alexBrewingStand, ClassType.BrewingStand,
-					"brewingStand.cooldown", "Brewing Stand");
+		if (!isPlayerAlive()) {
+//			if (runnable != null) runnable.cancel();
+			return;
 		}
-		
-		if (checkIfDead(player, instance) && runnable != null) {
-			runnable.cancel();
+
+		if (!brewAbility.isReady()) {
+			int remainingTime = (int) (brewAbility.getCooldownInstance().getRemainingCooldownSeconds() + 1);
+
+			String msg = ChatColorHelper.color( "&7Brewing " + potionDisplayNamePrefix + " &7in &e" + remainingTime + "s");
+			getActionBarManager().setActionBar(player, "brewing.cooldown", msg, 2);
+
+			hasSentBrewedMessage = false;
+		} else {
+			if (hasSentBrewedMessage) return;
+			String msg = ChatColorHelper.color("&7Brewed " + potionDisplayNamePrefix);
+			getActionBarManager().setActionBar(player, "brewing.cooldown", msg, 2);
+			hasSentBrewedMessage = true;
 		}
 	}
 
-	/*
-	 * This function checks if the player's 2nd slot is empty and if so, add back
-	 * the barrier till they get a new potion
-	 */
-	private void checkIfEmpty() {
-		ItemStack item = player.getInventory().getItem(1);
+	@Override
+	public void DoDamage(EntityDamageByEntityEvent event) {
+		if (!isPlayerAlive()) return;
+		if (!brewAbility.isReady()) return;
+		if (!(event.getEntity() instanceof Player)) return;
+		checkToAddBlazePowder(event);
+	}
 
-		if (item == null || item.getType() == Material.AIR) {
-			player.getInventory().setItem(1,
-					ItemHelper.setDetails(new ItemStack(Material.BARRIER), ChatColor.RED + "No potion item yet!"));
+	private void checkToAddBlazePowder(EntityDamageByEntityEvent event) {
+		ItemStack heldItem = player.getInventory().getItem(player.getInventory().getHeldItemSlot());
+
+		boolean isWeaponMelee =
+				event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK
+						&& heldItem != null
+						&& heldItem.equals(weapon);
+
+		if (!isWeaponMelee) return;
+
+		addBlazePowder();
+	}
+
+	private void addBlazePowder() {
+		ItemStack slot9 = player.getInventory().getItem(8);
+
+		if (slot9 == null) return;
+
+		if (slot9.getType() == Material.BARRIER) {
+			player.getInventory().setItem(8, new ItemStack(Material.BLAZE_POWDER));
+		} else {
+			if (slot9.getAmount() < 5) {
+				player.getInventory().addItem(new ItemStack(Material.BLAZE_POWDER));
+			}
 		}
 	}
 
 	@Override
 	public void UseItem(PlayerInteractEvent event) {
 		ItemStack item = event.getItem();
-		if (item != null && item.getType() == Material.BREWING_STAND_ITEM
-				&& (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK
-						|| event.getAction() == Action.LEFT_CLICK_AIR
-						|| event.getAction() == Action.LEFT_CLICK_BLOCK)) {
-			if (player.getGameMode() != GameMode.SPECTATOR) {
-				event.setCancelled(true);
-				if (alexBrewingStand.getTime() < 10000) {
-					int seconds = (10000 - alexBrewingStand.getTime()) / 1000 + 1;
-					player.sendMessage(color("&c&l(!) &rYou can't brew for another &e" + seconds + "s"));
-				} else {
-					if (player.getInventory().getItem(8).getType() == Material.BARRIER)
-						return;
+		Action action = event.getAction();
 
-					ItemStack blazePowder = player.getInventory().getItem(8);
-					this.alexBrewingStand.restart();
-					this.used = true;
-					player.sendMessage(color("&r&l(!) &rBrewing potion..."));
-					player.getInventory().setItem(8, ItemHelper.setDetails(new ItemStack(Material.BARRIER),
-							ChatColor.RED + "No blaze powder yet!"));
-					
-					runnable = new BukkitRunnable() {
-						@Override
-						public void run() {
-							if (checkIfDead(player, instance))
-								cancel();
-							
-							if (used)
-								potionsToGive(blazePowder);
-						}
-					};
-					runnable.runTaskLater(instance.getGameManager().getMain(), 4 * 20L);
-				}
-			}
+		if (item == null) return;
+
+		if (action != Action.RIGHT_CLICK_AIR && action != Action.RIGHT_CLICK_BLOCK) return;
+
+		if (!(item.equals(brewingItem))) return;
+		if (!brewAbility.isReady()) return;
+		if (player.getInventory().getItem(8).getType() == Material.BARRIER) {
+			brewAbility.sendCustomMessage("&c&l(!) &rNo blaze powder yet");
+			return;
 		}
+		brewAbility.use();
+		brewPotion();
+	}
+
+	private void brewPotion() {
+		int blazePowderAmount = player.getInventory().getItem(8).getAmount();
+		player.getInventory().setItem(8, barrierItem);
+		potionDisplayNamePrefix = getDisplayNamePrefix(blazePowderAmount);
+
+		runnable = new BukkitRunnable() {
+			@Override
+			public void run() {
+				if (!isPlayerAlive()) {
+					cancel();
+					return;
+				}
+
+				givePotion(blazePowderAmount);
+			}
+		};
+		runnable.runTaskLater(instance.getGameManager().getMain(), (long) (brewAbility.getCooldownDurationSeconds() * 20L));
 	}
 
 	/**
 	 * This function gives a potion to the player depending on how much blaze powder
 	 * they have in their inventory 8th slot
 	 */
-	private void potionsToGive(ItemStack blazePowder) {
-		potionItem = null;
-		Potion pot = new Potion(1);
-		int amount = blazePowder.getAmount();
+	private void givePotion(int blazePowderAmount) {
+		ItemStack potionItem = createPotion(blazePowderAmount);
+		if (potionItem == null) return;
+		player.getInventory().addItem(potionItem);
+		SoundManager.playSoundToPlayer(player, Sound.SWIM, 1, 0.5f);
+	}
 
-		if (blazePowder != null) {
-			if (amount == 1) {
-				potionItem = ItemHelper.setDetails(new ItemStack(Material.POTION, 1),
-						instance.getGameManager().getMain().color("&3&lSlowness II &7(15 sec)"));
-				pot.setSplash(true);
-				PotionMeta meta = (PotionMeta) potionItem.getItemMeta();
-				pot.setType(PotionType.SLOWNESS);
-				meta.addCustomEffect(new PotionEffect(PotionEffectType.SLOW, 15 * 20, 1), true);
-				potionItem.setItemMeta(meta);
-			} else if (amount == 2) {
-				potionItem = ItemHelper.setDetails(new ItemStack(Material.POTION, 1),
-						instance.getGameManager().getMain().color("&a&lJump VIII &7(20 sec)"));
-				pot.setSplash(true);
-				PotionMeta meta = (PotionMeta) potionItem.getItemMeta();
-				pot.setType(PotionType.JUMP);
-				meta.addCustomEffect(new PotionEffect(PotionEffectType.JUMP, 10 * 20, 7), true);
-				potionItem.setItemMeta(meta);
-			} else if (amount == 3) {
-				potionItem = ItemHelper.setDetails(new ItemStack(Material.POTION, 1),
-						instance.getGameManager().getMain().color("&b&lSpeed II &7(20 sec)"));
-				pot.setSplash(true);
-				PotionMeta meta = (PotionMeta) potionItem.getItemMeta();
-				pot.setType(PotionType.SPEED);
-				meta.addCustomEffect(new PotionEffect(PotionEffectType.SPEED, 20 * 20, 1), true);
-				potionItem.setItemMeta(meta);
-			} else if (amount == 4) {
-				potionItem = ItemHelper.setDetails(new ItemStack(Material.POTION, 1),
-						instance.getGameManager().getMain().color("&d&lRegen III &7(5 sec)"));
-				pot.setSplash(true);
-				PotionMeta meta = (PotionMeta) potionItem.getItemMeta();
-				pot.setType(PotionType.REGEN);
-				meta.addCustomEffect(new PotionEffect(PotionEffectType.REGENERATION, 5 * 20, 2), true);
-				potionItem.setItemMeta(meta);
-			} else if (amount == 5) {
-				potionItem = ItemHelper.setDetails(
-						new ItemStack(Material.POTION, 1),
-						"&f&lResistance " + (resistance.getAmplifier() + 1) + " &7(" + resistance.getDuration() + " sec)"
-				);
-				pot.setSplash(true);
-				PotionMeta meta = (PotionMeta) potionItem.getItemMeta();
-				pot.setType(PotionType.NIGHT_VISION);
-				meta.addCustomEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 15 * 20, 0, false, true), true);
-				potionItem.setItemMeta(meta);
-				resPotItem = potionItem;
-			} else if (amount == 6) {
-				potionItem = ItemHelper.setDetails(new ItemStack(Material.POTION, 1),
-						instance.getGameManager().getMain().color("&4&lStrength I &7(5 sec)"));
-				pot.setSplash(true);
-				PotionMeta meta = (PotionMeta) potionItem.getItemMeta();
-				pot.setType(PotionType.STRENGTH);
-				meta.addCustomEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 5 * 20, 0), true);
-				potionItem.setItemMeta(meta);
-			}
+	private ItemStack createPotion(int blazePowderAmount) {
+		PotionEffect effect = getPotionEffect(blazePowderAmount);
+		if (effect == null) return null;
 
-			pot.apply(potionItem);
-			player.getInventory().setItem(1, potionItem);
-			player.playSound(player.getLocation(), Sound.LEVEL_UP, 1, 10);
-			player.sendMessage(color("&2&l(!) &rYou brewed a " + potionItem.getItemMeta().getDisplayName() + " &rpotion!"));
-			this.used = false; // Reset Brewing Stand usage
+		String displayNamePrefix = getDisplayNamePrefix(blazePowderAmount);
+		return createPotionWithDynamicName(effect, displayNamePrefix);
+	}
+
+	private PotionEffect getPotionEffect(int blazePowderAmount) {
+		switch (blazePowderAmount) {
+			case 1: return jump;
+			case 2: return speed;
+			case 3: return strength;
+			case 4: return resistance;
+			case 5: return regeneration;
+			default: return null;
 		}
 	}
 
-	/**
-	 * When a person with Alex class hits a player, they will receive Brewing items
-	 * but a random item from a pool of different items
-	 */
-	@Override
-	public void DoDamage(EntityDamageByEntityEvent event) {
-		BaseClass bc = instance.classes.get(player);
-		if (bc != null && bc.getLives() <= 0) // If player is dead/spectator don't give items
-			return;
-
-		if (event.getEntity() instanceof Player) {
-			Player p = (Player) event.getEntity();
-			if (instance.duosMap != null)
-				if (instance.team.get(p).equals(instance.team.get(player)))
-					return;
-			
-			ItemStack slot9 = player.getInventory().getItem(8);
-			ItemStack slot2 = player.getInventory().getItem(1);
-			
-			if (slot2.getType() == Material.BARRIER) {
-				if (slot9 != null && slot9.getType() == Material.BARRIER) {
-					player.getInventory().setItem(8, new ItemStack(Material.BLAZE_POWDER));
-				} else {
-					if (slot9.getAmount() < 6)
-						player.getInventory().addItem(new ItemStack(Material.BLAZE_POWDER));
-				}
-			}
+	private String getDisplayNamePrefix(int blazePowderAmount) {
+		switch (blazePowderAmount) {
+			case 1: return "&a&lJump";
+			case 2: return "&b&lSpeed";
+			case 3: return "&4&lStrength";
+			case 4: return "&f&lResistance";
+			case 5: return "&d&lRegen";
+			default: return "";
 		}
 	}
 
-//	@Override
-//	public void PotionSplashEvent(PotionSplashEvent event) {
-//		if (event.getEntity() == null) return;
-//		ThrownPotion potion = event.getPotion();
-//		if (potion == null) return;
-//		ItemStack item = potion.getItem();
-//		if (!(item.equals(resPotItem))) return;
-//		for (Entity affectedEntity : event.getAffectedEntities()) {
-//			if (!(affectedEntity instanceof Player)) continue;
-//			Player player1 = (Player) affectedEntity;
-//			if (!player1.equals(player)) continue;
-//
-//			player1.addPotionEffect(resistance);
-//		}
-//	}
+	private ItemStack createPotionWithDynamicName(PotionEffect effect, String displayNamePrefix) {
+		// Dynamically generate the display name with amplifier and duration
+		String displayName = displayNamePrefix + " " + getRomanNumeral(effect.getAmplifier() + 1) +
+				" &7(" + (effect.getDuration() / 20) + " sec)";
+
+		// Create the potion item with the dynamic display name
+		return ItemHelper.setDetails(
+				ItemHelper.createPotionItem(effect, true, true),
+				displayName
+		);
+	}
+
+	private String getRomanNumeral(int number) {
+		switch (number) {
+			case 1: return "I";
+			case 2: return "II";
+			case 3: return "III";
+			case 4: return "IV";
+			case 5: return "V";
+			case 6: return "VI";
+			case 7: return "VII";
+			case 8: return "VIII";
+			case 9: return "IX";
+			case 10: return "X";
+			default: return "" + number; // Fallback for numbers > 10
+		}
+	}
 
 	@Override
 	public ClassType getType() {
@@ -267,11 +279,7 @@ public class BrewingStandClass extends BaseClass {
 
 	@Override
 	public ItemStack getAttackWeapon() {
-		ItemStack sword = ItemHelper.addEnchant(new ItemStack(Material.WOOD_SWORD), Enchantment.KNOCKBACK, 1);
-		ItemMeta meta = sword.getItemMeta();
-		meta.spigot().setUnbreakable(true);
-		sword.setItemMeta(meta);
-		return sword;
+		return weapon;
 	}
 
 	private ItemStack getBrewingStand() {
@@ -280,7 +288,7 @@ public class BrewingStandClass extends BaseClass {
 				"",
 				color("&rHit players to obtain Brewing items"),
 				color("&rthen right click to get a potion"),
-				"",
+
 				color("&7 - 1 Powder: &eSlowness II (15 sec)"),
 				color("&7 - 2 Powder: &eJump 8 (10 sec)"),
 				color("&7 - 3 Powder: &eSpeed II (20 sec)"),
