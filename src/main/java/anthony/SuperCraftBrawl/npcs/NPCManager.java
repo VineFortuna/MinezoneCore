38de553a42eb38d80d72f772cb2d68849ec3c528
MinezoneMC <136758983+MinezoneMC@users.noreply.github.com>
2025-10-12 17:56:49 -0400

    Fixed new NPC locations

--- src/main/java/anthony/SuperCraftBrawl/npcs/NPCManager.java
+++ src/main/java/anthony/SuperCraftBrawl/npcs/NPCManager.java
@@ -28,7 +28,7 @@ public class NPCManager implements Listener {
 	private NPCLib npcLib;
 
 	// NPCS HERE:
-	private NPC fishing, scbModes, socialMedia;
+	private NPC fishing, scbClassic, scbDuels, socialMedia, parkour;
 
 	public NPCManager(Core main) {
 		this.main = main;
@@ -38,43 +38,44 @@ public class NPCManager implements Listener {
 	}
 
 	private void load() {
-		int skinId = 277513;
-		
-		// if (main.lobbyWorld.getName().equals("lobbies")) {
 		Skin sethblingSkin = new Skin(
 				"ewogICJ0aW1lc3RhbXAiIDogMTYzOTc5ODIxNjM4OCwKICAicHJvZmlsZUlkIiA6ICI1NWEyZTcyZTAxNjE0Yzg1YjE5MWEwYjIyN2ZmNzU4YSIsCiAgInByb2ZpbGVOYW1lIiA6ICJTZXRoQmxpbmciLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZWEzZTg3OTcxYTZmYjhlM2Q5NzZjN2RhYjYzMjU3MmVlMjAwZDI4MmE0NGVkYTVkMGEyYTNjNDE1MjQ2NGQyZiIKICAgIH0sCiAgICAiQ0FQRSIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMTc5MTI3OTBmZjE2NGI5MzE5NmYwOGJhNzFkMGU2MjEyOTMwNDc3NmQwZjM0NzMzNGY4YTZlYWU1MDlmOGE1NiIKICAgIH0KICB9Cn0=",
 				"tSB+tKhVQKKut1lm4VETD1l/ulDTI40F2f//L3d6pMZP2LGRp/B8BNbvqYjWdzY80aqw9t2zlvkAGQGFLOF1QPlCoA+OgG9kSS+OP7CZhM3fzTLjpIVyQQtKxCTJuh664Oj4JuzgfEYq/Yc7emUj7/asazPlNvgpY0BbS0pxXrhiN47UoNYjkxoBuyOX+uIkhmRzO+bQH1rRrMMdY6CYgubyz6QJXNJqg4zBgeRaUZHbtxlpy6FRcQPrvteacznCyfFa/cpkbbVLMPD9NQxCuB4bt29ygfVAgcO6+o5O/1JMJfA3yKpp0xuayZ6ILgIRzIBPP1943IAmegspjH8cs08Hhu7G9HuP7fyhvE3e8jMGzhhoeFmB4nZXDJxzT5ALsHjhzEzZFs3VazdiiaJy+7GwkqxQD6Y5NNUrMuHhvLo3nK9vs0kLjS/aQhBl4CkRIaqA3fJoE/ctpniJ5KVyaoSrF5YOn3QFp4qUg9JFr0KU7Ld3L2bcPwSFEIU7V3nIi7Kza9yx2JDOXOzmApfdDz1pYRB/g3D7WdVk1rRb/xPVUTlAjss4b9ijLhIHhcKGQyNl2dXPiIVnlx+FmJACnR40YZExLXEiFeutfUw1T8IP/Qqkg5158sKfqRPLwhvCqAjjqbDYtscPrNB1ApQCHTaL+UepPei+TFeHproVUHU=");
 		Skin fishermanSkin = new Skin(
 				"ewogICJ0aW1lc3RhbXAiIDogMTYxNDE3ODA2NzIzNywKICAicHJvZmlsZUlkIiA6ICJmZDYwZjM2ZjU4NjE0ZjEyYjNjZDQ3YzJkODU1Mjk5YSIsCiAgInByb2ZpbGVOYW1lIiA6ICJSZWFkIiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2I2MTY0YTNjM2JlYjFiYzAzMTAwMzBmMjgxNTU4OTE5ODEzZjBhMTVjMmQ3Y2I3NzIyNGMxZDk0ZmZmMjE0NDQiCiAgICB9CiAgfQp9",
 				"GsrpTxzgihotEuasiq0cefZtiIzhyASNyAcXiHfFI+HqsC8aAZP/P6HYRa7zC3iQqMvDaVMZYFnmBui3B4KC49STqXy7UheGPT5xylV3dfjurinwpOUYJMNRcJYW9pLb98jVZ+bQW9LKHUdRbxgkGJxpJQQ+LQlROfpZ9DaM/fdVjImiMSUyUeCF5WyMxadRrEWeo+a1Cn25IrNWo+KWQy7OWLJONeZcuboZGqobuckZKfIRvW1Vtt2v45PXRI5ec/6YO6kr+yGVS51xlAZOzxjaaGACA+/5gHLeEu0Oka/hdGY0HdukqKcg3TXf1cZZ7qB/VCpnAuWOguTfXU09gh2BGRvOizsQpL5tRpg4lA/l5Q70t2jtj+79M2yRYC7LQs/SrUUIX6VJ3w8ylKKqTjLE+PVCmgwgW1bYX3tpt9xEhRJ5kPv47iX0DkVcGxE2QW3xd8gyJfE5BzeKhcXjEV+GJNa2NOiHnVIKJPuj+trJnOiKDainhcR00+Ncna5I9TomGm2r0TGUrO/bPqW7b2yvpBNUnAweUYjU/eG2E9uY5IzVPwO9M+PqulqD5Reqq3MW32WAcmLkO417Tn48tZnAmYjVoM5zb16ZN/J9LQgHMf9L0/ZgguBgKOx0UQeedJjwgkA9BZdL6LuTiLwfcEt2ZxSwvvAXfBWVmrSlVns=");
-		
-		scbModes = npcLib.createNPC(Arrays.asList("" + ChatColor.AQUA + ChatColor.BOLD + "JOIN GAME", "",
-				main.color("&7Click to join a SCB game")));
-		scbModes.setLocation(new Location(main.getLobbyWorld(), 192.506, 113, 649.530, 179, -0));
-		scbModes.setSkin(sethblingSkin);
-		scbModes.setItem(NPCSlot.MAINHAND, ItemHelper.create(Material.COMPASS));
-		scbModes.create();
-		
+
+		scbClassic = npcLib.createNPC(Arrays.asList(main.color("&b&lSUPER CRAFT BROS"), main.color("&eMode: &bClassic"),
+				main.color("&7Click to Play!")));
+		scbClassic.setLocation(new Location(main.getLobbyWorld(), 191.5, 106, 657.5, 174, 2));
+		scbClassic.setSkin(sethblingSkin);
+		scbClassic.setItem(NPCSlot.MAINHAND, ItemHelper.create(Material.COMPASS));
+		scbClassic.create();
+
+		scbDuels = npcLib.createNPC(Arrays.asList(main.color("&b&lSUPER CRAFT BROS"), main.color("&eMode: &bDuels"),
+				main.color("&7Click to Play!")));
+		scbDuels.setLocation(new Location(main.getLobbyWorld(), 187.5, 106, 657.5, -174, 3));
+		scbDuels.setSkin(sethblingSkin);
+		scbDuels.setItem(NPCSlot.MAINHAND, ItemHelper.create(Material.COMPASS));
+		scbDuels.create();
+
+		parkour = npcLib.createNPC(Arrays.asList(main.color("&b&lPARKOUR"), main.color("&7Click to Play!")));
+		parkour.setLocation(new Location(main.getLobbyWorld(), 183.5, 106, 655.5, -158, 2));
+		parkour.setSkin(sethblingSkin);
+		parkour.create();
+
 		socialMedia = npcLib.createNPC(Arrays.asList("" + ChatColor.AQUA + ChatColor.BOLD + "View Social Medias", "",
 				main.color("&7Click to check the list!")));
 		socialMedia.setLocation(new Location(main.getLobbyWorld(), 192.962, 115.5, 632.989, 137, 10));
 		socialMedia.setSkin(sethblingSkin);
 		socialMedia.create();
-		
-		fishing = npcLib.createNPC(
-				Arrays.asList("" + ChatColor.DARK_AQUA + ChatColor.BOLD + "FISHING", "",
-						main.color("&7Click to go fishing")));
-		fishing.setLocation(new Location(main.getLobbyWorld(), 186.506, 113, 649.530, -179, -0));
+
+		fishing = npcLib.createNPC(Arrays.asList(main.color("&b&lFISHING"),
+				main.color("&7Click to go fishing")));
+		fishing.setLocation(new Location(main.getLobbyWorld(), 195.5, 106, 655.5, 162, 3));
 		fishing.setSkin(fishermanSkin);
 		fishing.setItem(NPCSlot.MAINHAND, ItemHelper.create(Material.FISHING_ROD));
 		fishing.create();
-		
-		// FIX IT SO THIS UPDATES EVERY 10 SECONDS
-		//}
-		//);
-		
-		//update();
-		// }
 	}
 
 	private void update() { // Updates NPCs
@@ -142,27 +143,23 @@ public class NPCManager implements Listener {
 //			}
 //			player.sendPluginMessage(main, "BungeeCord", b.toByteArray());
 //		} else
-			/*if (e.getNPC() == parkour) {
-			if (main.p.hasPlayer(player)) {
-				player.sendMessage(main.color("&r&l(!) &rHere is your reward: &e300 Tokens"));
-				player.sendMessage(main.color("&r&l(!) &rTime Taken: &e" + main.p.time.get(player)));
-				PlayerData data = main.getDataManager().getPlayerData(player);
-				main.p.runnables.remove(player);
-				main.p.time.remove(player);
-				main.getParkour().players.remove(player);
-				main.ResetPlayer(player);
-				main.LobbyItems(player);
-				player.setAllowFlight(true);
-
-				if (data != null) {
-					data.tokens += 300;
-					main.getScoreboardManager().lobbyBoard(player);
-					main.SendPlayerToHub(player);
-				}
-			}*/
+		/*
+		 * if (e.getNPC() == parkour) { if (main.p.hasPlayer(player)) {
+		 * player.sendMessage(main.color("&r&l(!) &rHere is your reward: &e300 Tokens"))
+		 * ; player.sendMessage(main.color("&r&l(!) &rTime Taken: &e" +
+		 * main.p.time.get(player))); PlayerData data =
+		 * main.getDataManager().getPlayerData(player); main.p.runnables.remove(player);
+		 * main.p.time.remove(player); main.getParkour().players.remove(player);
+		 * main.ResetPlayer(player); main.LobbyItems(player);
+		 * player.setAllowFlight(true);
+		 * 
+		 * if (data != null) { data.tokens += 300;
+		 * main.getScoreboardManager().lobbyBoard(player); main.SendPlayerToHub(player);
+		 * } }
+		 */
 		if (e.getNPC() == fishing) {
 			new FishingGUI(main, null).inv.open(player);
-		} else if (e.getNPC() == scbModes) {
+		} else if (e.getNPC() == scbClassic) {
 			new GameSelectorGUI(main).inv.open(player);
 		} else if (e.getNPC() == socialMedia) {
 			player.sendMessage(main.color("&8&m-------&8[Social Media]&8&m-------"));
@@ -183,10 +180,12 @@ public class NPCManager implements Listener {
 		// if (main.lobbyWorld.getName().equals("lobbies")) {
 		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(main, () -> {
 			Bukkit.getScheduler().runTask(main, () -> fishing.show(e.getPlayer()));
-			Bukkit.getScheduler().runTask(main, () -> scbModes.show(e.getPlayer()));
+			Bukkit.getScheduler().runTask(main, () -> scbClassic.show(e.getPlayer()));
 			Bukkit.getScheduler().runTask(main, () -> socialMedia.show(e.getPlayer()));
+			Bukkit.getScheduler().runTask(main, () -> scbDuels.show(e.getPlayer()));
+			Bukkit.getScheduler().runTask(main, () -> parkour.show(e.getPlayer()));
 		}, 20L);
-		//}
+		// }
 	}
 
 	/*
