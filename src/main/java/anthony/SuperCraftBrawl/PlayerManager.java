5a7af07d0a1f1381f74d07da1f6ea95d7bdaf001
MinezoneMC <136758983+MinezoneMC@users.noreply.github.com>
2025-10-15 13:22:03 -0400

    Updated rank tags to uppercase, changed color scheme of scoreboard & lobby items

---
+++ src/main/java/anthony/SuperCraftBrawl/PlayerManager.java
@@ -0,0 +1,17 @@
+package anthony.SuperCraftBrawl;
+
+
+import org.bukkit.entity.Player;
+
+public class PlayerManager {
+
+    private Core core;
+    private Player player;
+
+    public PlayerManager(Core core, Player player) {
+        this.core = core;
+        this.player = player;
+    }
+
+
+}
