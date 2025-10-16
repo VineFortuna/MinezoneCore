39e78f9b89b2215a812c7974be6ed789e96d269c
MinezoneMC <136758983+MinezoneMC@users.noreply.github.com>
2025-10-16 01:09:45 -0400

    Fixed a bunch of memory leaks (probably not everything, but a good chunk), Updated parkour scoreboard to match consistency with other scoreboards, "A Game on NightDragon is starting in 30 seconds, Click here to join". This now only now displays to players in the lobby and not the whole server and lastly, Did lots of code refactoring myself to help improve readability

--- src/main/java/anthony/SuperCraftBrawl/Bars.java
+++ src/main/java/anthony/SuperCraftBrawl/Bars.java
@@ -73,7 +73,7 @@ public final class Bars { //
         sb.append(net.md_5.bungee.api.ChatColor.DARK_GRAY).append(' ');
 
         if (showPercent) {
-            sb.append(ChatColor.AQUA)
+            sb.append(ChatColor.GREEN)
               .append((int) Math.round(pct * 100)).append('%'); // tight "79%"
         }
         return sb.toString();
