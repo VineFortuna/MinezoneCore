fd9b54a1395e9ad7769b2ee95953dbc33235c88f
MinezoneMC <136758983+MinezoneMC@users.noreply.github.com>
2026-06-05 18:27:49 -0400

    Added StaffHelpManager.java for better code readability

---
+++ src/main/java/anthony/SuperCraftBrawl/staffhelp/StaffHelpManager.java
@@ -0,0 +1,20 @@
+package anthony.SuperCraftBrawl.staffhelp;
+
+import anthony.SuperCraftBrawl.Core;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StaffHelpManager {
+
+    private Core core;
+    public String staffhelp = "";
+    public String staffhelpReply = "";
+
+    public StaffHelpManager(Core core) {
+        this.core = core;
+    }
+}
