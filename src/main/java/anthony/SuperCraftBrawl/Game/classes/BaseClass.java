7b8e8fc1a799a846bce0bc1c3d59cf5e45fcefba
Auggiethecat <noahthomassymonds@gmail.com>
2026-06-14 16:41:03 -0400

    SCB: classes: SnowGolem: fix barrier bug where it wouldn't respawn after using a slowball after getting it back from a kill after using all of them.

--- src/main/java/anthony/SuperCraftBrawl/Game/classes/BaseClass.java
+++ src/main/java/anthony/SuperCraftBrawl/Game/classes/BaseClass.java
@@ -1851,21 +1851,7 @@ public abstract class BaseClass {
 				damagerPlayer.getInventory().setItem(2, zombieEgg);
 			} else if (baseClass.getType() == ClassType.Wolf) {
 				wolfPackAdd = true;
-			} else if (baseClass.getType() == ClassType.SnowGolem) {
-                ItemStack slowballs = ItemHelper.setDetails(
-                        new ItemStack(Material.SNOW_BALL, 1),
-                        "&f&lSLOWBALL &7(Right click)",
-                        "&7Hit players within &e2 blocks &7to give:",
-                        "&7Slowness 2 for 3s + half a heart damage"
-                );
-
-                if (damagerPlayer.getInventory().getItem(2).getType() != Material.SNOW_BALL)
-                    damagerPlayer.getInventory().setItem(2, slowballs);
-                else
-                    damagerPlayer.getInventory().addItem(slowballs);
-
-                damagerPlayer.sendMessage(instance.color("&2&l(!) &rYou got a kill and gained an extra &f&lSLOWBALL"));
-            }
+			}
 		}
 	}
 
