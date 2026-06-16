1a8767e4d175722e88747ae7f80d3b8b3681cc1f
Anthony <anthonyciceu71@gmail.com>
2026-06-16 18:05:28 -0400

    Nerfed damage of 'Rising Ruin'

--- src/main/java/anthony/SuperCraftBrawl/Game/classes/all/HerobrineClass.java
+++ src/main/java/anthony/SuperCraftBrawl/Game/classes/all/HerobrineClass.java
@@ -1,13 +1,14 @@
 package anthony.SuperCraftBrawl.Game.classes.all;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import anthony.SuperCraftBrawl.Game.classes.Ability;
 import anthony.util.ChatColorHelper;
 import org.bukkit.Effect;
-import org.bukkit.EntityEffect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -19,12 +20,18 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -42,23 +49,25 @@ public class HerobrineClass extends BaseClass {
     private final ItemStack despairItem;
     private final Ability despairAbility = new Ability("&b&lDiamond of Despair", DESPAIR_ABILITY_COOLDOWN / 1000, player);
     private static final double DESPAIR_ABILITY_COOLDOWN = 15 * 1000;
-    private static final double DESPAIR_ABILITY_RANGE    = 10;
+    private static final double DESPAIR_ABILITY_RANGE = 10;
 
     private final PotionEffect poison = new PotionEffect(PotionEffectType.POISON, 4 * 20, 1, true, true);
 
     // Rising Ruin constants
     private static final double RUIN_EXPLOSION_RADIUS = 3.5;
-    private static final double RUIN_DAMAGE_SELF      = 3.0;
-    private static final double RUIN_DAMAGE_ENEMY     = 8.0;
-    private static final double RUIN_UPWARD_VELOCITY  = 0.95;
+    private static final double RUIN_DAMAGE_SELF = 3.0;
+    private static final double RUIN_DAMAGE_ENEMY = 5.5;
+    private static final double RUIN_UPWARD_VELOCITY = 0.95;
+
+    private static final String RUIN_METADATA = "RisingRuinBlock";
 
     private static final int[][] RUIN_CIRCLE = {
-            {-1, -3}, {0, -3}, {1, -3}, {2, -3},   // top row
+            {-1, -3}, {0, -3}, {1, -3}, {2, -3},
             {-2, -2},                         {3, -2},
             {-2, -1},                         {3, -1},
             {-2,  0},                         {3,  0},
             {-2,  1},                         {3,  1},
-            {-1,  2}, {0,  2}, {1,  2}, {2,  2}    // bottom row
+            {-1,  2}, {0,  2}, {1,  2}, {2,  2}
     };
 
     public HerobrineClass(GameInstance instance, Player player) {
@@ -119,9 +128,10 @@ public class HerobrineClass extends BaseClass {
         ItemStack item = event.getItem();
         if (item != null && item.getType() == Material.DIAMOND
                 && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) {
+            event.setCancelled(true);
+
             if (herobrine.getTime() < DESPAIR_ABILITY_COOLDOWN) {
                 int seconds = (int) ((DESPAIR_ABILITY_COOLDOWN - herobrine.getTime()) / 1000 + 1);
-                event.setCancelled(true);
                 player.sendMessage(instance.color("&c&l(!) &rYour &b&lDiamond of Despair&r is still " +
                         "regenerating for &a" + seconds + "s"));
             } else {
@@ -174,10 +184,6 @@ public class HerobrineClass extends BaseClass {
         }
     }
 
-    // -----------------------------------------------------------------------
-    //  Zombie Summon
-    // -----------------------------------------------------------------------
-
     private void spawnZombies(List<Player> targets) {
         player.sendMessage(ChatColorHelper.color("&2&l(!) &rYou summoned zombies upon your enemies!"));
 
@@ -193,10 +199,6 @@ public class HerobrineClass extends BaseClass {
             zombie.setCanPickupItems(false);
         }
     }
-
-    // -----------------------------------------------------------------------
-    //  Rising Ruin
-    // -----------------------------------------------------------------------
 
     @SuppressWarnings("deprecation")
     private void launchRisingRuin(List<Player> targets, int wave) {
@@ -214,8 +216,39 @@ public class HerobrineClass extends BaseClass {
         }
 
         final Player caster = this.player;
-        final int[] landedCount = { 0 };
+        final int[] landedCount = {0};
         final int totalBlocks = RUIN_CIRCLE.length;
+        final String sessionId = "ruin-" + player.getUniqueId() + "-" + System.nanoTime() + "-" + wave;
+
+        final List<List<RuinBlockSnapshot>> allSnapshots = new ArrayList<>();
+        final Map<Integer, RuinEntityData> ruinEntities = new HashMap<>();
+
+        final Listener ruinListener = new Listener() {
+            @EventHandler
+            public void onRisingRuinChangeBlock(EntityChangeBlockEvent event) {
+                if (!isRisingRuinEntity(event.getEntity(), sessionId)) {
+                    return;
+                }
+
+                event.setCancelled(true);
+
+                RuinEntityData data = ruinEntities.get(event.getEntity().getEntityId());
+
+                if (data != null) {
+                    cleanupRuinBlocks(data.blockMat, data.snapshots);
+                    scheduleExtraRuinCleanup(data.blockMat, data.snapshots);
+                }
+
+                resendActualBlock(event.getBlock());
+
+                if (!event.getEntity().isDead()) {
+                    event.getEntity().remove();
+                }
+            }
+        };
+
+        instance.getGameManager().getMain().getServer().getPluginManager()
+                .registerEvents(ruinListener, instance.getGameManager().getMain());
 
         for (int i = 0; i < RUIN_CIRCLE.length; i++) {
             Location spawnLoc = new Location(
@@ -226,15 +259,19 @@ public class HerobrineClass extends BaseClass {
             );
 
             final Material blockMat = (i % 2 == 0) ? Material.NETHERRACK : Material.SOUL_SAND;
+            final List<RuinBlockSnapshot> originalBlocks = captureRuinBlockSnapshot(world, spawnLoc);
+            allSnapshots.add(originalBlocks);
 
             FallingBlock fb = world.spawnFallingBlock(spawnLoc, blockMat, (byte) 0);
             fb.setVelocity(new Vector(0, RUIN_UPWARD_VELOCITY, 0));
             fb.setDropItem(false);
             fb.setHurtEntities(false);
+            fb.setMetadata(RUIN_METADATA, new FixedMetadataValue(instance.getGameManager().getMain(), sessionId));
 
-            final Location storedLoc = spawnLoc.clone();
-            final Location[] lastPos = { fb.getLocation() };
-            final boolean[] triggered = { false };
+            ruinEntities.put(fb.getEntityId(), new RuinEntityData(blockMat, originalBlocks));
+
+            final Location[] lastPos = {fb.getLocation()};
+            final boolean[] triggered = {false};
 
             new BukkitRunnable() {
                 @Override
@@ -242,13 +279,8 @@ public class HerobrineClass extends BaseClass {
                     if (triggered[0]) return;
 
                     triggered[0] = true;
-
-                    if (fb.isValid() && !fb.isDead()) {
-                        fb.remove();
-                    }
-
-                    removePlacedRuinBlocks(blockMat, lastPos[0], storedLoc);
-                    onBlockLanded(world, lastPos[0], caster, targets, landedCount, totalBlocks, wave);
+                    completeRuinBlock(fb, blockMat, originalBlocks, lastPos[0], world,
+                            caster, targets, landedCount, totalBlocks, wave, ruinListener, ruinEntities);
                 }
             }.runTaskLater(instance.getGameManager().getMain(), 100L);
 
@@ -258,8 +290,8 @@ public class HerobrineClass extends BaseClass {
                     if (!fb.isValid() || fb.isDead()) {
                         if (!triggered[0]) {
                             triggered[0] = true;
-                            removePlacedRuinBlocks(blockMat, lastPos[0], storedLoc);
-                            onBlockLanded(world, lastPos[0], caster, targets, landedCount, totalBlocks, wave);
+                            completeRuinBlock(fb, blockMat, originalBlocks, lastPos[0], world,
+                                    caster, targets, landedCount, totalBlocks, wave, ruinListener, ruinEntities);
                         }
 
                         this.cancel();
@@ -274,12 +306,10 @@ public class HerobrineClass extends BaseClass {
                         Location below = current.clone().subtract(0, 0.2, 0);
 
                         if (below.getBlock().getType().isSolid() || below.getY() <= origin.getY() + 0.5) {
-                            fb.remove();
-
                             if (!triggered[0]) {
                                 triggered[0] = true;
-                                removePlacedRuinBlocks(blockMat, current, storedLoc);
-                                onBlockLanded(world, current, caster, targets, landedCount, totalBlocks, wave);
+                                completeRuinBlock(fb, blockMat, originalBlocks, current, world,
+                                        caster, targets, landedCount, totalBlocks, wave, ruinListener, ruinEntities);
                             }
 
                             this.cancel();
@@ -288,57 +318,206 @@ public class HerobrineClass extends BaseClass {
                 }
             }.runTaskTimer(instance.getGameManager().getMain(), 5L, 1L);
         }
+
+        /*
+         * Final safety cleanup. If Bukkit/client weirdness leaves a ghost block,
+         * this forces the real server block state back to every player again.
+         */
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Map.Entry<Integer, RuinEntityData> entry : ruinEntities.entrySet()) {
+                    RuinEntityData data = entry.getValue();
+                    cleanupRuinBlocks(data.blockMat, data.snapshots);
+                }
+
+                for (List<RuinBlockSnapshot> snapshots : allSnapshots) {
+                    resendRuinSnapshots(snapshots);
+                }
+
+                HandlerList.unregisterAll(ruinListener);
+            }
+        }.runTaskLater(instance.getGameManager().getMain(), 140L);
     }
 
-    private void removePlacedRuinBlocks(Material blockMat, Location... locations) {
-        if (locations == null) return;
+    private void completeRuinBlock(FallingBlock fb,
+                                   Material blockMat,
+                                   List<RuinBlockSnapshot> originalBlocks,
+                                   Location impactLoc,
+                                   World world,
+                                   Player caster,
+                                   List<Player> targets,
+                                   int[] landedCount,
+                                   int totalBlocks,
+                                   int wave,
+                                   Listener ruinListener,
+                                   Map<Integer, RuinEntityData> ruinEntities) {
+        if (fb != null) {
+            ruinEntities.remove(fb.getEntityId());
 
-        for (Location loc : locations) {
-            if (loc == null || loc.getWorld() == null) continue;
+            if (fb.isValid() && !fb.isDead()) {
+                fb.remove();
+            }
+        }
 
-            World world = loc.getWorld();
-            int x = loc.getBlockX();
-            int z = loc.getBlockZ();
-            int baseY = loc.getBlockY();
+        cleanupRuinBlocks(blockMat, originalBlocks);
+        scheduleExtraRuinCleanup(blockMat, originalBlocks);
+
+        Location safeImpact = impactLoc == null ? player.getLocation() : impactLoc.clone();
+        onBlockLanded(world, safeImpact, caster, targets, landedCount, totalBlocks, wave, ruinListener);
+    }
+
+    private boolean isRisingRuinEntity(Entity entity, String sessionId) {
+        if (entity == null || !entity.hasMetadata(RUIN_METADATA)) {
+            return false;
+        }
+
+        for (MetadataValue value : entity.getMetadata(RUIN_METADATA)) {
+            if (value != null && sessionId.equals(value.asString())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @SuppressWarnings("deprecation")
+    private List<RuinBlockSnapshot> captureRuinBlockSnapshot(World world, Location spawnLoc) {
+        List<RuinBlockSnapshot> snapshots = new ArrayList<>();
+
+        if (world == null || spawnLoc == null) {
+            return snapshots;
+        }
+
+        int x = spawnLoc.getBlockX();
+        int z = spawnLoc.getBlockZ();
+        int baseY = spawnLoc.getBlockY();
+
+        /*
+         * Capture the whole possible vertical path of the falling block.
+         * This lets us safely restore or resend blocks without deleting map terrain.
+         */
+        for (int y = baseY - 4; y <= baseY + 32; y++) {
+            if (y < 0 || y >= world.getMaxHeight()) continue;
+
+            Block block = world.getBlockAt(x, y, z);
+            snapshots.add(new RuinBlockSnapshot(world, x, y, z, block.getType(), block.getData()));
+        }
+
+        return snapshots;
+    }
+
+    @SuppressWarnings("deprecation")
+    private void cleanupRuinBlocks(Material blockMat, List<RuinBlockSnapshot> snapshots) {
+        if (snapshots == null) return;
+
+        for (RuinBlockSnapshot snapshot : snapshots) {
+            Block block = snapshot.world.getBlockAt(snapshot.x, snapshot.y, snapshot.z);
+
+            boolean currentIsRuinBlock = block.getType() == blockMat;
+            boolean mapAlreadyHadSameBlock = snapshot.originalType == blockMat
+                    && snapshot.originalData == block.getData();
 
             /*
-             * FallingBlock sometimes settles slightly above/below the entity's last
-             * position, especially on non-full collision blocks like:
-             *
-             * - fences
-             * - cobblestone walls
-             * - iron bars
-             * - glass panes
-             * - fence gates
-             * - slabs/stairs
-             *
-             * So instead of only checking one exact block, check a small vertical
-             * column and remove the temporary Rising Ruin block wherever Bukkit placed it.
+             * Only restore if Rising Ruin actually placed/ghosted its own block here.
+             * If the map already had netherrack/soul sand here, leave it alone.
              */
-            for (int y = baseY - 2; y <= baseY + 2; y++) {
-                Block block = world.getBlockAt(x, y, z);
-
-                if (block.getType() == blockMat) {
-                    block.setType(Material.AIR);
-                }
+            if (currentIsRuinBlock && !mapAlreadyHadSameBlock) {
+                block.setTypeIdAndData(snapshot.originalType.getId(), snapshot.originalData, false);
             }
+
+            resendActualBlock(block);
         }
     }
 
-    private void onBlockLanded(World world, Location impactLoc, Player caster,
-                               List<Player> targets, int[] landedCount, int totalBlocks, int wave) {
-        triggerRuinImpact(world, impactLoc, caster, targets);
-        landedCount[0]++;
+    private void scheduleExtraRuinCleanup(final Material blockMat, final List<RuinBlockSnapshot> snapshots) {
+        int[] delays = {1, 3, 10, 20};
 
-        if (landedCount[0] >= totalBlocks && wave == 1) {
+        for (final int delay : delays) {
             new BukkitRunnable() {
                 @Override
                 public void run() {
-                    if (player.isOnline()) {
-                        launchRisingRuin(targets, 2);
-                    }
+                    cleanupRuinBlocks(blockMat, snapshots);
+                    resendRuinSnapshots(snapshots);
                 }
-            }.runTaskLater(instance.getGameManager().getMain(), 10L);
+            }.runTaskLater(instance.getGameManager().getMain(), delay);
+        }
+    }
+
+    private void resendRuinSnapshots(List<RuinBlockSnapshot> snapshots) {
+        if (snapshots == null) return;
+
+        for (RuinBlockSnapshot snapshot : snapshots) {
+            Block block = snapshot.world.getBlockAt(snapshot.x, snapshot.y, snapshot.z);
+            resendActualBlock(block);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void resendActualBlock(Block block) {
+        if (block == null || block.getWorld() == null) {
+            return;
+        }
+
+        Location location = block.getLocation();
+
+        for (Player onlinePlayer : block.getWorld().getPlayers()) {
+            onlinePlayer.sendBlockChange(location, block.getType(), block.getData());
+        }
+    }
+
+    private static class RuinEntityData {
+        private final Material blockMat;
+        private final List<RuinBlockSnapshot> snapshots;
+
+        private RuinEntityData(Material blockMat, List<RuinBlockSnapshot> snapshots) {
+            this.blockMat = blockMat;
+            this.snapshots = snapshots;
+        }
+    }
+
+    private static class RuinBlockSnapshot {
+        private final World world;
+        private final int x;
+        private final int y;
+        private final int z;
+        private final Material originalType;
+        private final byte originalData;
+
+        private RuinBlockSnapshot(World world, int x, int y, int z, Material originalType, byte originalData) {
+            this.world = world;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.originalType = originalType;
+            this.originalData = originalData;
+        }
+    }
+
+    private void onBlockLanded(World world,
+                               Location impactLoc,
+                               Player caster,
+                               List<Player> targets,
+                               int[] landedCount,
+                               int totalBlocks,
+                               int wave,
+                               Listener ruinListener) {
+        triggerRuinImpact(world, impactLoc, caster, targets);
+        landedCount[0]++;
+
+        if (landedCount[0] >= totalBlocks) {
+            HandlerList.unregisterAll(ruinListener);
+
+            if (wave == 1) {
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        if (player.isOnline()) {
+                            launchRisingRuin(targets, 2);
+                        }
+                    }
+                }.runTaskLater(instance.getGameManager().getMain(), 10L);
+            }
         }
     }
 
