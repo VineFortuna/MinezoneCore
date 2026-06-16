a4294138bdb804ee0cf0bb04bf57002ff79358fa
Anthony <anthonyciceu71@gmail.com>
2026-06-16 18:06:51 -0400

    Added function to remove and re-add EntityArmorStand, removed original one from Core.java for refactoring

--- src/main/java/anthony/SuperCraftBrawl/mysterychest/MysteryChestManager.java
+++ src/main/java/anthony/SuperCraftBrawl/mysterychest/MysteryChestManager.java
@@ -2,17 +2,22 @@ package anthony.SuperCraftBrawl.mysterychest;
 
 import anthony.SuperCraftBrawl.Core;
 import anthony.SuperCraftBrawl.gui.MysteryChestsGUI;
+import anthony.SuperCraftBrawl.playerdata.PlayerData;
 import net.md_5.bungee.api.ChatColor;
+import net.minecraft.server.v1_8_R3.EntityArmorStand;
+import net.minecraft.server.v1_8_R3.PacketPlayOutEntityDestroy;
+import net.minecraft.server.v1_8_R3.PacketPlayOutSpawnEntityLiving;
+import net.minecraft.server.v1_8_R3.WorldServer;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MysteryChestManager implements Listener {
 
@@ -27,25 +32,53 @@ public class MysteryChestManager implements Listener {
     @EventHandler
     public void mysteryChest(PlayerInteractEvent e) {
         Player player = e.getPlayer();
-        List<Material> list = new ArrayList<>(Arrays.asList(Material.CHEST));
+
+        if (e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (e.getClickedBlock() == null) return;
+        if (e.getClickedBlock().getType() != Material.CHEST) return;
 
         if (player.getWorld() == main.getLobbyWorld()) {
-            if (e.getAction() == Action.RIGHT_CLICK_BLOCK && list.contains(e.getClickedBlock().getType())) {
-                if (chestCanOpen == false) {
-                    e.setCancelled(true);
-                    new MysteryChestsGUI(main, e.getClickedBlock().getLocation()).inv.open(player);
-                } else {
-                    e.setCancelled(true);
-                    player.sendMessage("" + ChatColor.LIGHT_PURPLE + ChatColor.BOLD + "(!) " + ChatColor.RESET
-                            + "This MysteryChest is already in use!");
-                }
-            }
-        } else {
-            if (e.getAction() == Action.RIGHT_CLICK_BLOCK && list.contains(e.getClickedBlock().getType())) {
-                // e.setCancelled(true);
-                // REMOVE LATER
+            e.setCancelled(true);
+
+            if (!chestCanOpen) {
+                new MysteryChestsGUI(main, e.getClickedBlock().getLocation()).inv.open(player);
+            } else {
+                player.sendMessage("" + ChatColor.LIGHT_PURPLE + ChatColor.BOLD + "(!) " + ChatColor.RESET
+                        + "This MysteryChest is already in use!");
             }
         }
     }
 
+    public void removeAndAddMysteryStand(Player player) {
+        PlayerData data = main.getDataManager().getPlayerData(player);
+        if (data == null) return;
+
+        World lobby = main.getLobbyWorld();
+        if (lobby == null) return;
+
+        if (player.getWorld() != lobby) return;
+
+        EntityArmorStand oldStand = main.msHologram.get(player);
+
+        if (oldStand != null) {
+            PacketPlayOutEntityDestroy destroyPacket = new PacketPlayOutEntityDestroy(oldStand.getId());
+            ((CraftPlayer) player).getHandle().playerConnection.sendPacket(destroyPacket);
+        }
+
+        Location loc = new Location(lobby, 198.5, 105.2, 650.5);
+
+        WorldServer worldServer = ((CraftWorld) lobby).getHandle();
+        EntityArmorStand newStand = new EntityArmorStand(worldServer);
+
+        newStand.setLocation(loc.getX(), loc.getY(), loc.getZ(), 0, 0);
+        newStand.setCustomName(main.color("&a" + data.mysteryChests + " &rto open!"));
+        newStand.setCustomNameVisible(true);
+        newStand.setGravity(false);
+        newStand.setInvisible(true);
+
+        PacketPlayOutSpawnEntityLiving spawnPacket = new PacketPlayOutSpawnEntityLiving(newStand);
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(spawnPacket);
+
+        main.msHologram.put(player, newStand);
+    }
 }
